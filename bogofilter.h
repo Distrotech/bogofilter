8a3fc672b769ac7685a3fd59cc6f459ceb53151e
relson <relson@046ba1d7-8659-4f4f-8137-08d775d845e8>
2002-09-15 19:07:13 +0000

    Add an enumerated type for return codes of RC_SPAM and RC_NONSPAM, which  values of 0 and 1 as called for by procmail. Use the new codes and type for bogofilter() and when generating the X-Spam-Status message.
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/trunk/bogofilter@16 046ba1d7-8659-4f4f-8137-08d775d845e8

--- bogofilter.h
+++ bogofilter.h
@@ -1,9 +1,15 @@
 /* $Id$ */
 /* $Log$
- * Revision 1.1  2002/09/14 22:15:20  adrian_otto
- * Initial revision
- * */
+ * Revision 1.2  2002/09/15 19:07:12  relson
+ * Add an enumerated type for return codes of RC_SPAM and RC_NONSPAM, which  values of 0 and 1 as called for by procmail.
+ * Use the new codes and type for bogofilter() and when generating the X-Spam-Status message.
+ *
+/* Revision 1.1.1.1  2002/09/14 22:15:20  adrian_otto
+/* 0.7.3 Base Source
+/* */
 //  constants and declarations for bogofilter
+
+typedef enum rc_e {RC_SPAM=0, RC_NONSPAM=1}  rc_t;
 
 typedef struct 
 {
@@ -20,12 +26,11 @@ extern void write_list(wordlist_t *ham_list);
 extern void register_words(int fd, wordlist_t *list, wordlist_t *other);
 extern int get_token(void);
 extern void lexer_stream_mode(void);
-extern int bogofilter(int fd);
+extern rc_t bogofilter(int fd);
 extern int bogodump(char *file);
 
 extern wordlist_t ham_list, spam_list;
 extern int verbose;
-extern int status;
 
 // discard tokens longer than this, so as not to clutter up our
 // wordlists with a lot of MIME-enclosure cruft.
@@ -35,7 +40,7 @@ extern int status;
 #define TOKEN	1	// Ordinary token
 #define FROM	2	// Mail message delimiter
 extern FILE	*yyin;
-extern char *yytext;
+extern char	*yytext;
 
 struct textblock
 {
