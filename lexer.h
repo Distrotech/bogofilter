b733eb68ded44ef23b8efe92be44ee4b62ba4776
relson <relson@046ba1d7-8659-4f4f-8137-08d775d845e8>
2002-09-18 22:30:22 +0000

    Created lexer.h with the definitions needed by lexer_l.l from bogofilter.h. This removes the compile-time dependency between the two files.
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/trunk/bogofilter@46 046ba1d7-8659-4f4f-8137-08d775d845e8

---
+++ lexer.h
@@ -0,0 +1,18 @@
+// discard tokens longer than this, so as not to clutter up our
+// wordlists with a lot of MIME-enclosure cruft.
+#define MAXWORDLEN	20
+
+// lexer interface
+#define TOKEN	1	// Ordinary token
+#define FROM	2	// Mail message delimiter
+extern FILE	*yyin;
+extern char	*yytext;
+
+struct textblock
+{
+    char		*block;
+    struct textblock	*next;
+};
+
+extern struct textblock textblocks, *textend;
+
