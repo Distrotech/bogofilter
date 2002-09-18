58e458fa8dea4595c312ab750b38cd3a63d9ec99
relson <relson@046ba1d7-8659-4f4f-8137-08d775d845e8>
2002-09-18 22:30:22 +0000

    Created lexer.h with the definitions needed by lexer_l.l from bogofilter.h. This removes the compile-time dependency between the two files.
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/trunk@46 046ba1d7-8659-4f4f-8137-08d775d845e8

--- bogofilter/bogofilter.h
+++ bogofilter/bogofilter.h
@@ -1,13 +1,19 @@
 /* $Id$ */
 /* $Log$
- * Revision 1.2  2002/09/15 19:07:12  relson
- * Add an enumerated type for return codes of RC_SPAM and RC_NONSPAM, which  values of 0 and 1 as called for by procmail.
- * Use the new codes and type for bogofilter() and when generating the X-Spam-Status message.
+ * Revision 1.3  2002/09/18 22:30:22  relson
+ * Created lexer.h with the definitions needed by lexer_l.l from bogofilter.h.
+ * This removes the compile-time dependency between the two files.
  *
+/* Revision 1.2  2002/09/15 19:07:12  relson
+/* Add an enumerated type for return codes of RC_SPAM and RC_NONSPAM, which  values of 0 and 1 as called for by procmail.
+/* Use the new codes and type for bogofilter() and when generating the X-Spam-Status message.
+/*
 /* Revision 1.1.1.1  2002/09/14 22:15:20  adrian_otto
 /* 0.7.3 Base Source
 /* */
 //  constants and declarations for bogofilter
+
+#include "lexer.h"
 
 typedef enum rc_e {RC_SPAM=0, RC_NONSPAM=1}  rc_t;
 
@@ -31,23 +37,5 @@ extern int bogodump(char *file);
 
 extern wordlist_t ham_list, spam_list;
 extern int verbose;
-
-// discard tokens longer than this, so as not to clutter up our
-// wordlists with a lot of MIME-enclosure cruft.
-#define MAXWORDLEN	20
-
-// lexer interface
-#define TOKEN	1	// Ordinary token
-#define FROM	2	// Mail message delimiter
-extern FILE	*yyin;
-extern char	*yytext;
-
-struct textblock
-{
-    char		*block;
-    struct textblock	*next;
-};
-
-extern struct textblock textblocks, *textend;
 
 // end
