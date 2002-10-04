0c1fab07e928e2db14f3d3964d9ee9fe5f44781a
m-a <m-a@046ba1d7-8659-4f4f-8137-08d775d845e8>
2002-10-04 00:31:30 +0000

    Add convenience function xstrdup() which is a xmalloc() variant of strdup.
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/trunk@108 046ba1d7-8659-4f4f-8137-08d775d845e8

---
+++ bogofilter/xstrdup.c
@@ -0,0 +1,17 @@
+/* (C) 2002 by Matthias Andree, redistributable according to the terms
+ * of the GNU General Public License, v2.
+ *
+ * $Id$
+ *
+ * $History$
+ *
+ */
+
+#include <string.h>
+#include "xmalloc.h"
+
+char *xstrdup(const char *s) {
+    char *t = xmalloc(strlen(s) + 1);
+    strcpy(t, s);
+    return t;
+}
