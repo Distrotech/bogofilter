8b9e9c22b8ee5be9c5396fe3371e98e960161319
m-a <m-a@046ba1d7-8659-4f4f-8137-08d775d845e8>
2002-10-04 01:40:37 +0000

    Add C++ support, and if only to run code through a C++ compile for stricter type checking...
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/trunk/bogofilter@116 046ba1d7-8659-4f4f-8137-08d775d845e8

--- common.h
+++ common.h
@@ -1,7 +1,12 @@
 #ifndef COMMON_H_GUARD
 #define COMMON_H_GUARD
 
+#ifndef __cplusplus
 typedef enum bool { FALSE = 0, TRUE = 1 } bool;
+#else
+const bool FALSE = false;
+const bool TRUE = true;
+#endif
 
 #define max(x, y)	(((x) > (y)) ? (x) : (y))
 #define min(x, y)	(((x) < (y)) ? (x) : (y))
