d043374084a25907ca8f389e530fa7a41022aa33
m-a <m-a@046ba1d7-8659-4f4f-8137-08d775d845e8>
2002-10-04 00:39:57 +0000

    First set of type fixes.
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/trunk/bogofilter@109 046ba1d7-8659-4f4f-8137-08d775d845e8

--- wordlists.h
+++ wordlists.h
@@ -1,6 +1,9 @@
 /* $Id$ */
 /* 
  * $Log$
+ * Revision 1.3  2002/10/04 00:39:57  m-a
+ * First set of type fixes.
+ *
  * Revision 1.2  2002/10/02 17:14:54  relson
  * main.c now calls setup_lists() for initializing the wordlist structures, including the opening of the wordlist.db files.
  * setup_list() takes a directory name as its argument and passes it to init_list(), which calls open_wordlist() for the actual open.
@@ -20,7 +23,7 @@ struct wordlist_s
     wordlist_t *next;
     char *name;			// resource name (for debug/verbose messages)
     void *dbh;			// database handle 
-    unsigned long msgcount;	// count of messages in wordlist.
+    long msgcount;	// count of messages in wordlist.
     char *file;			// associated  file
     double weight;
     bool bad;
