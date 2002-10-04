9d0ffb00289ed996017c358dae12e171d54fb4cc
gyepi <gyepi@046ba1d7-8659-4f4f-8137-08d775d845e8>
2002-10-04 01:29:00 +0000

    header file for shared definitions and such
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/trunk/bogofilter@111 046ba1d7-8659-4f4f-8137-08d775d845e8

---
+++ common.h
@@ -0,0 +1,9 @@
+#ifndef COMMON_H_GUARD
+#define COMMON_H_GUARD
+
+typedef enum bool { FALSE = 0, TRUE = 1 } bool;
+
+#define max(x, y)	(((x) > (y)) ? (x) : (y))
+#define min(x, y)	(((x) < (y)) ? (x) : (y))
+
+#endif
