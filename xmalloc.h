efa9a64c019dd7b6b8dcea3a789b2778736c6ac9
m-a <m-a@046ba1d7-8659-4f4f-8137-08d775d845e8>
2002-10-04 00:45:19 +0000

    Make this splint clean and add splint comments.
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/trunk/bogofilter@110 046ba1d7-8659-4f4f-8137-08d775d845e8

--- xmalloc.h
+++ xmalloc.h
@@ -2,10 +2,13 @@
 #define XMALLOC_H_GUARD
 #include <stdlib.h>
 
+/*@only@*//*@out@*//*@notnull@*/
 void *xmalloc(size_t size);
-void xfree(void *ptr);
+void xfree(/*@only@*/ void *ptr);
+/*@only@*//*@out@*//*@notnull@*/
 void *xcalloc(size_t nmemb, size_t size);
-void *xrealloc(void *ptr, size_t size);
+/*@only@*//*@out@*//*@notnull@*/
+void *xrealloc(/*@only@*/ void *ptr, size_t size);
 
 #endif
 
