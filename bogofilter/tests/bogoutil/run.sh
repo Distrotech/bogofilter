9cf1b5cc479a572c665b8302fb0ea91d62fa4831
m-a <m-a@046ba1d7-8659-4f4f-8137-08d775d845e8>
2002-10-15 11:41:54 +0000

    Fix: make distcheck works now.
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/trunk@197 046ba1d7-8659-4f4f-8137-08d775d845e8

--- bogofilter/tests/bogoutil/run.sh
+++ bogofilter/tests/bogoutil/run.sh
@@ -7,48 +7,39 @@
 #Test 4 -- Same as 2 except that BDB file has a  .MSG_COUNT token.
 #          Technically already upgraded.
 
-set -ex
+set -e
 
-for num in `seq 1 4`;do
-echo "Running test $num"
-(
-	inputfile="input-${num}"
-	for ext in txt count; do
-	  t="${inputfile}.$ext" 
-	  if [ -f $srcdir/$t ]; then
-	     inputfile=$t
-	   break
-	  fi
-	done
+tdir=`mktemp -d run.sh.$$.XXXXXXXXXX`
+trap "a=\$? ; rm -f -r $tdir && exit \$a" 0 1 2 3 15
 
-	if [ ! -f "$srcdir/$inputfile" ]; then
-	  exit 2;
-	fi
-
-	killinput=0
-	if [ "$srcdir" != "." ] ; then
-	    cp -f $srcdir/$inputfile x$inputfile
-	    inputfile=x$inputfile
-	    killinput=1
-	fi
-	
-	datafile="${srcdir}/input-${num}-data.txt"
-	inputdb="input-${num}.db"
-
-	if [ -f "$datafile" ]; then
-		rm -f $inputdb
-		../../bogoutil -l $inputdb <  $datafile
-	fi
-
-	outputdb="output-${num}.db"
-	rm -f $outputdb
-
-	perl ${srcdir}/../../bogoupgrade.pl -b ../../bogoutil -i $inputfile -o $outputdb && \
-	../../bogoutil -d $outputdb |LC_COLLATE=C sort\
-	|diff -u - ${srcdir}/output-${num}.txt
-	rm -f $outputdb
-	rm -f $inputdb
-	[ $killinput -eq 0 ] || rm -f $inputfile
-) || { echo "failed test $num" ; exit 1 ; }
+inputfile="input-${num}"
+for ext in txt count; do
+  t="${inputfile}.$ext" 
+  if [ -f $srcdir/$t ]; then
+     inputfile=$t
+   break
+  fi
 done
 
+if [ ! -f "$srcdir/$inputfile" ]; then
+  exit 2;
+fi
+
+cp -f $srcdir/$inputfile $tdir/
+
+datafile="${srcdir}/input-${num}-data.txt"
+inputdb="input-${num}.db"
+
+if [ -f "$datafile" ]; then
+	rm -f $tdir/$inputdb
+	../../bogoutil -l $tdir/$inputdb <  $datafile
+fi
+
+outputdb="output-${num}.db"
+rm -f $tdir/$outputdb
+
+perl ${srcdir}/../../bogoupgrade.pl -b ../../bogoutil \
+    -i $tdir/$inputfile -o $tdir/$outputdb \
+&& ../../bogoutil -d $tdir/$outputdb \
+|LC_COLLATE=C sort\
+|diff -u - ${srcdir}/output-${num}.txt
