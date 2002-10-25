58521bec37996a0cb8aefd1777294768f4a57603
m-a <m-a@046ba1d7-8659-4f4f-8137-08d775d845e8>
2002-10-25 01:15:08 +0000

    Split #ifdef MAIN from debug.c to debug.main.c. Change #include from strings.h to string.h
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/trunk/bogofilter@268 046ba1d7-8659-4f4f-8137-08d775d845e8

--- debug.c
+++ debug.c
@@ -12,7 +12,8 @@ AUTHOR:
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <strings.h>
+#include <string.h>
+
 #include "debug.h"
 
 bit_t debug_mask = DEBUG_NONE;
@@ -33,36 +34,3 @@ void set_debug_mask( char *mask )
 	}
     }
 }
-
-#ifdef	MAIN
-typedef struct mask_char_to_symbol_s {
-    char  *str;
-    bit_t bit;
-}  mask_char_to_symbol_t;
-
-mask_char_to_symbol_t char_to_symbol[] =
-{
-    { "g",	BIT_GENERAL },
-    { "s",	BIT_SPAMICITY },
-    { "d",	BIT_DATABASE },
-    { "l",	BIT_LEXER },
-    { "w",	BIT_WORDLIST }
-};
-
-int main(int argc, char **argv)
-{
-    int i;
-    for (i=0; i<sizeof(char_to_symbol)/sizeof(char_to_symbol[0]); i += 1)
-    {
-	mask_char_to_symbol_t *ptr = char_to_symbol + i;
-	set_debug_mask( ptr->str );
-	if ( (debug_mask & ptr->bit) != ptr->bit )
-	{
-	    fprintf(stderr, "debug_mask for '%s' is wrong.\n", ptr->str);
-	    exit(2);
-	}
-    }
-    printf("All O.K.\n");
-    return 0;
-}
-#endif
