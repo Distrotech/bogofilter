83ff2dc1521b051f2a8428679b21b4dc0931f141
relson <relson@046ba1d7-8659-4f4f-8137-08d775d845e8>
2002-10-28 21:29:31 +0000

    Added a passthrough option "-p".  Useful for simply seeing the words or for piping to bogoutil to get spam/good counts.
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/trunk/bogofilter@319 046ba1d7-8659-4f4f-8137-08d775d845e8

--- lexertest.c
+++ lexertest.c
@@ -8,29 +8,50 @@
 /* exports */
 int passthrough;
 
+void help(void);
+
 int main(int argc, char **argv)
 {
-    int	t, quiet = 0;
+    char *arg;
+    int	t;
+    int count=0;
+    int quiet = 0;
+    int passthru = 0;
 
-    if (argc >= 2 && argv[1] && 0 == strcmp(argv[1], "-h")) {
-	puts("Usage: lexertest [options]");
-	puts("Options are:");
-	puts("  -h    help, this output.");
-	puts("  -q    quiet mode, no tokens are printed.");
-	exit(0);
+    while ((arg = *++argv))
+    {
+	if (strcmp(arg, "-h") == 0)
+	    help();
+	quiet += strcmp(arg, "-q") == 0;
+	passthru += strcmp(arg, "-p") == 0;
     }
 
-    if (argc >= 2 && argv[1] && 0 == strcmp(argv[1], "-q")) quiet=1;
+    if (!passthru)
+	if (quiet)
+	    puts( "quiet mode.");
+	else
+	    puts("normal mode.");
 
-    if (quiet) {
-	puts("quiet mode.");
-	while ((t = get_token()) > 0) { }
-    } else {
-	puts("normal mode.");
-	while ((t = get_token()) > 0)
-	{
+    while ((t = get_token()) > 0)
+    {
+	count += 1;
+	if ( passthru )
+	    (void) printf("%s\n", yylval);
+	else if (!quiet)
 	    (void) printf("get_token: %d '%s'\n", t, yylval);
-	}
     }
+    if ( !passthru )
+	printf( "%d tokens read.\n", count );
+
     return 0;
+}
+
+void help(void)
+{
+    puts("Usage: lexertest [options]");
+    puts("Options are:");
+    puts("  -h    help, this output.");
+    puts("  -q    quiet mode, no tokens are printed.");
+    puts("  -p    print the tokens.");
+    exit(0);
 }
