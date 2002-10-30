5933c8459ca4a516146b8fc8cf0a3033ec138e70
relson <relson@046ba1d7-8659-4f4f-8137-08d775d845e8>
2002-10-30 17:21:15 +0000

    *** empty log message ***
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/trunk/bogofilter@350 046ba1d7-8659-4f4f-8137-08d775d845e8

--- debug.h
+++ debug.h
@@ -6,8 +6,8 @@
 #ifdef	NODEBUG
 typedef enum bit_e {DEBUG_NONE=0}  bit_t;
 #else
-#define MASKNAMES "gldsw"
-typedef enum bit_e {DEBUG_NONE=0, BIT_GENERAL=1, BIT_LEXER=2, BIT_DATABASE=4, BIT_SPAMICITY=8, BIT_WORDLIST=16}  bit_t;
+#define MASKNAMES "gldswc"
+typedef enum bit_e {DEBUG_NONE=0, BIT_GENERAL=1, BIT_LEXER=2, BIT_DATABASE=4, BIT_SPAMICITY=8, BIT_WORDLIST=16, BIT_CONFIG=32}  bit_t;
 #endif
 
 extern bit_t debug_mask;
@@ -18,12 +18,14 @@ extern bit_t debug_mask;
 #define DEBUG_DATABASE(level)	0
 #define DEBUG_LEXER(level)	0
 #define DEBUG_WORDLIST(level)	0
+#define DEBUG_CONFIG(level)	0
 #else
 #define	DEBUG_GENERAL(level) 	((debug_mask & BIT_GENERAL)   && (verbose > level))
 #define DEBUG_SPAMICITY(level)	((debug_mask & BIT_SPAMICITY) && (verbose > level))
 #define DEBUG_DATABASE(level)	((debug_mask & BIT_DATABASE)  && (verbose > level))
 #define DEBUG_LEXER(level)	((debug_mask & BIT_LEXER)     && (verbose > level))
 #define DEBUG_WORDLIST(level)	((debug_mask & BIT_WORDLIST)  && (verbose > level))
+#define DEBUG_CONFIG(level)	((debug_mask & BIT_CONFIG)    && (verbose > level))
 #endif
 
 void set_debug_mask( char *mask );
