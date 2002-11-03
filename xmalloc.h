ff73a8eb770f6ff0b4294566682d886e39c8fe4c
m-a <m-a@046ba1d7-8659-4f4f-8137-08d775d845e8>
2002-11-03 17:29:21 +0000

    Fix splint annotations, insert blank lines to improve legibility.
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/trunk/bogofilter@378 046ba1d7-8659-4f4f-8137-08d775d845e8

--- xmalloc.h
+++ xmalloc.h
@@ -6,9 +6,12 @@
 
 /*@only@*//*@out@*//*@notnull@*/
 void *xmalloc(size_t size);
-void xfree(/*@only@*/ void *ptr);
+
+void xfree(/*@only@*//*@null@*/ void *ptr);
+
 /*@only@*//*@out@*//*@notnull@*/
 void *xcalloc(size_t nmemb, size_t size);
+
 /*@only@*//*@out@*//*@notnull@*/
 void *xrealloc(/*@only@*/ void *ptr, size_t size);
 
