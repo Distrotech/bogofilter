f8129dd48d08a443caf829316999d6b6bca6706e
relson <relson@046ba1d7-8659-4f4f-8137-08d775d845e8>
2002-11-10 15:40:47 +0000

    Added cvs Id: tags at start of file.
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/trunk@502 046ba1d7-8659-4f4f-8137-08d775d845e8

--- bogofilter/xstrdup.h
+++ bogofilter/xstrdup.h
@@ -1,3 +1,5 @@
+/* $Id$ */
+
 /* (C) 2002 by Matthias Andree, redistributable according to the terms
  * of the GNU General Public License, v2.
  *
@@ -9,5 +11,7 @@
 
 #ifndef XSTRDUP_H_GUARD
 #define XSTRDUP_H_GUARD
+
 char *xstrdup(const char *s);
+
 #endif
