2955b2d491dc0a77d01f7d361ed479046da22402
m-a <m-a@046ba1d7-8659-4f4f-8137-08d775d845e8>
2002-11-24 18:16:08 +0000

    Split xmalloc for library use.
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/trunk@718 046ba1d7-8659-4f4f-8137-08d775d845e8

---
+++ bogofilter/xcalloc.c
@@ -0,0 +1,14 @@
+/* $Id$ */
+
+#include <stdlib.h>
+#include "xmalloc.h"
+
+void
+*xcalloc(size_t nmemb, size_t size){
+   void *x;
+   x = calloc(nmemb, size);
+   if (x == NULL) {
+       xmem_error("xcalloc");
+   }
+   return x;
+}
