b459ff6005c2e52e1f5f6bd309b215f344793217
m-a <m-a@046ba1d7-8659-4f4f-8137-08d775d845e8>
2002-11-25 20:54:25 +0000

    Restructure build process. More dependencies are moving into the libbogofilter.a library, for easier Makefile.am maintenance.
    Restructure registration process. We now have two wordhashes, one
    per-message (with properly capped frequencies), and one global; after
    each message, the per-message wordhash is moved to the global wordhash
    and freed. After the EOF marker has been read, the global wordhash is
    registered.
    
    NOTE IMPORTANT API CHANGE: the per-message wordhash is context
    sensitive, it skips the very first FROM token to avoid emitting a bogus
    message with 0 (zero) tokens. If bogofilter be amended to treat multiple
    mailboxes at some time in the future, you MUST call collect_reset to
    reset the "first FROM token" indicator.
    
    Add the non-installed and undocumented bogowordfreq test program, it
    reads the stdin, parses it for tokens, counts their number of
    occurrences, capping at the max_frequency. This defaults to 1, but can
    be overriden with a positive integer argument on the command line.
    
    
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/trunk@755 046ba1d7-8659-4f4f-8137-08d775d845e8

--- bogofilter/register.h
+++ bogofilter/register.h
@@ -6,7 +6,6 @@
 
 #include <wordhash.h>
 
-extern void *collect_words(int *message_count, int *word_count);
 extern void register_messages(run_t _run_type);
 extern void register_words(run_t _run_type, wordhash_t *h,
 			   int msgcount, int wordcount);
