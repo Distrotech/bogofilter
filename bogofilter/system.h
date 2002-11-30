cebf548aa6d490b67b9cd388f11d32e01953a99e
m-a <m-a@046ba1d7-8659-4f4f-8137-08d775d845e8>
2002-11-30 15:36:15 +0000

    Pull in Todd C. Miller's strlcpy/strlcat functions, from NetBSD basesrc/lib/libc/string/ CVS.
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/trunk@817 046ba1d7-8659-4f4f-8137-08d775d845e8

--- bogofilter/system.h
+++ bogofilter/system.h
@@ -37,5 +37,12 @@ char *strchr (), *strrchr ();
 # endif
 #endif
 
+#if !HAVE_STRLCPY
+size_t strlcpy(char *dst, const char *src, size_t size);
+#endif
+
+#if !HAVE_STRLCAT
+size_t strlcat(char *dst, const char *src, size_t size);
+#endif
 
 #endif
