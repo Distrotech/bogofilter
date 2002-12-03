0b067bdd92da8572a45d75be7a1ab041b8b59358
relson <relson@046ba1d7-8659-4f4f-8137-08d775d845e8>
2002-12-03 04:49:25 +0000

    Add token types (enum token_t) for NONE, IPADDR, and CHARSET.
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/trunk@887 046ba1d7-8659-4f4f-8137-08d775d845e8

--- bogofilter/lexer.h
+++ bogofilter/lexer.h
@@ -1,13 +1,19 @@
 /* $Id$ */
 
+#ifndef	HAVE_LEXER_H
+#define	HAVE_LEXER_H
+
 /* length of *yylval will not exceed this... */
 #define MAXTOKENLEN	30
 
 /* lexer interface */
 typedef enum {
-    TOKEN = 1,	/* regular token */
+    NONE = 0,
+    TOKEN,	/* regular token */
     FROM,	/* mbox message delimiter */
     BOUNDARY,	/* MIME multipart boundary line */
+    IPADDR,	/* ip address */
+    CHARSET,	/* charset="..." */
     EMPTY	/* empty line */
 } token_t;
 
@@ -25,3 +31,5 @@ extern struct textblock textblocks, *textend;
 extern int lexer_fgets(char *buf, int max_size, FILE *s);
 extern token_t get_token(void);
 extern token_t yylex(void);
+
+#endif	/* HAVE_LEXER_H */
