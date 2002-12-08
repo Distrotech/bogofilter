d0fad135d9e3a50c3a81c53a2f4aa2d9adf46c9a
relson <relson@046ba1d7-8659-4f4f-8137-08d775d845e8>
2002-12-08 21:10:10 +0000

    Add cvs headers and normalize indentation in fgetsl.c
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/trunk@988 046ba1d7-8659-4f4f-8137-08d775d845e8

--- bogofilter/fgetsl.c
+++ bogofilter/fgetsl.c
@@ -1,3 +1,12 @@
+/* $Id$ */
+
+/*****************************************************************************
+
+NAME:
+   fgetsl.c -- fgets(), returning length.
+
+******************************************************************************/
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -6,27 +15,27 @@
 /* calls exit(2) on read error or when max_size < 2 */
 int fgetsl(char *buf, int max_size, FILE *s)
 {
-	int c;
-	char *cp = buf;
+    int c;
+    char *cp = buf;
 
-	if (max_size < 2) {
-	    fprintf(stderr, "Invalid buffer size, exiting.\n");
-	    exit(2);
-	}
-	if (feof(s))
-	    return(EOF);
+    if (max_size < 2) {
+	fprintf(stderr, "Invalid buffer size, exiting.\n");
+	exit(2);
+    }
+    if (feof(s))
+	return(EOF);
 
-	while ((--max_size > 0) && ((c = getc(s)) != EOF)) {
-		*cp++ = c;
-		if (c == '\n')
-			break;
-	}
+    while ((--max_size > 0) && ((c = getc(s)) != EOF)) {
+	*cp++ = c;
+	if (c == '\n')
+	    break;
+    }
 
-	if (ferror(s)) {
-	    perror("stdin");
-	    exit(2);
-	}
+    if (ferror(s)) {
+	perror("stdin");
+	exit(2);
+    }
 
-	*cp = '\0'; /* DO NOT ADD ++ HERE! */
-	return(cp - buf);
+    *cp = '\0'; /* DO NOT ADD ++ HERE! */
+    return(cp - buf);
 }
