cb0e50d28619af0928cdb29c955f361fcd7bb773
m-a <m-a@046ba1d7-8659-4f4f-8137-08d775d845e8>
2002-12-08 20:51:56 +0000

    Clean up and split lexer_fgets as fgetsl() function into separate file. (think File-GET-String, returning Length).
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/trunk/bogofilter@987 046ba1d7-8659-4f4f-8137-08d775d845e8

---
+++ fgetsl.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "fgetsl.h"
+
+/* calls exit(2) on read error or when max_size < 2 */
+int fgetsl(char *buf, int max_size, FILE *s)
+{
+	int c;
+	char *cp = buf;
+
+	if (max_size < 2) {
+	    fprintf(stderr, "Invalid buffer size, exiting.\n");
+	    exit(2);
+	}
+	if (feof(s))
+	    return(EOF);
+
+	while ((--max_size > 0) && ((c = getc(s)) != EOF)) {
+		*cp++ = c;
+		if (c == '\n')
+			break;
+	}
+
+	if (ferror(s)) {
+	    perror("stdin");
+	    exit(2);
+	}
+
+	*cp = '\0'; /* DO NOT ADD ++ HERE! */
+	return(cp - buf);
+}
---
+++ fgetsl.h
@@ -0,0 +1,8 @@
+#ifndef FGETSL_H
+#define FGETSL_H
+
+#include <stdio.h>
+
+extern int fgetsl(char *, int, FILE *);
+
+#endif
