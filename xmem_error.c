fe0f5deff85fa13573fe6f9268035b05d6a1965e
m-a <m-a@046ba1d7-8659-4f4f-8137-08d775d845e8>
2002-12-13 15:30:47 +0000

    Add xstrlcpy and xstrlcat that catch overrun and abort the program.
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/trunk/bogofilter@1014 046ba1d7-8659-4f4f-8137-08d775d845e8

--- xmem_error.c
+++ xmem_error.c
@@ -9,3 +9,10 @@ void xmem_error(const char *a)
     (void)fprintf(stderr, "%s: Out of memory\n", a);
     abort();
 }
+
+/*@noreturn@*/
+void xmem_toolong(const char *a)
+{
+    (void)fprintf(stderr, "%s: String too long\n", a);
+    abort();
+}
