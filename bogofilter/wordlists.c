a505e4408320322e00e5eeb16601d5f5ee592845
m-a <m-a@046ba1d7-8659-4f4f-8137-08d775d845e8>
2002-12-19 22:33:46 +0000

    Switch to print_error, PRINT_ERROR is not portable.
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/trunk@1062 046ba1d7-8659-4f4f-8137-08d775d845e8

--- bogofilter/wordlists.c
+++ bogofilter/wordlists.c
@@ -29,7 +29,7 @@ void *open_wordlist( const char *name, const char *filepath )
     dbmode_t open_mode = (run_type==RUN_NORMAL) ? DB_READ : DB_WRITE;
 
     if ( (dbh = db_open(filepath, name, open_mode)) == NULL){
-      PRINT_ERROR("Cannot initialize database %s.", name);
+      print_error(__FILE__, __LINE__, "Cannot initialize database %s.", name);
       exit(2);
     }
 
