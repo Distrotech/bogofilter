00bf88c65c391c0d34d59ad96fa23c0097b90795
relson <relson@046ba1d7-8659-4f4f-8137-08d775d845e8>
2002-12-25 01:10:01 +0000

    Move path related functions to their own files.
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/trunk@1082 046ba1d7-8659-4f4f-8137-08d775d845e8

---
+++ bogofilter/paths.h
@@ -0,0 +1,22 @@
+/* $Id$ */
+
+/*****************************************************************************
+
+NAME:
+   paths.h -- prototypes for paths.c.
+
+******************************************************************************/
+
+#ifndef PATHS_H
+#define PATHS_H
+
+#include <stdlib.h>
+
+int build_path(char* dest, size_t size, const char* dir, const char* file);
+
+char *create_path_from_env(const char *var,
+			   /*@null@*/ const char *subdir);
+
+int check_directory(const char* path); /*@globals errno,stderr@*/
+
+#endif	/* PATHS_H */
