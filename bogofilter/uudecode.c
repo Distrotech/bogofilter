25bc657f2c2277b51f14f48cf8f8e7e40e082480
relson <relson@046ba1d7-8659-4f4f-8137-08d775d845e8>
2002-12-29 19:18:14 +0000

    Fix uudecode error.
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/branches/mime@1136 046ba1d7-8659-4f4f-8137-08d775d845e8

--- bogofilter/uudecode.c
+++ bogofilter/uudecode.c
@@ -36,7 +36,7 @@ int uudecode(byte *buff, size_t size)
 	int i;
 	for (i = 0; i < 4; i += 1) {
 	    byte c = *s++;
-	    v = v << 6 | (c - 0x20);
+	    v = v << 6 | ((c - 0x20) & 0x3F);
 	}
 	for (i = 2; i >= 0; i -= 1) {
 	    byte c = v & 0xFF;
