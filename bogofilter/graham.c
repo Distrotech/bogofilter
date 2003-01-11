9c130d10b650cbe21812466f8bcd191ab3c34300
relson <relson@046ba1d7-8659-4f4f-8137-08d775d845e8>
2003-01-11 18:32:29 +0000

    Recode floating point compare to potentional rounding problem.
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/branches/mime@1256 046ba1d7-8659-4f4f-8137-08d775d845e8

--- bogofilter/graham.c
+++ bogofilter/graham.c
@@ -89,8 +89,8 @@ static int compare_extrema(const void *id1, const void *id2)
     const discrim_t *d1 = id1;
     const discrim_t *d2 = id2;
 
-    if (d1->prob > d2->prob) return 1;
-    if (d1->prob < d2->prob) return -1;
+    if (d1->prob - d2->prob > EPS) return 1;
+    if (d2->prob - d1->prob > EPS) return -1;
 
     return strcmp(d1->key, d2->key);
 }
