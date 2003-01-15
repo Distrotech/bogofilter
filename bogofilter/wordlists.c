f276e4e56caddd85477553e5ec3d67664e6a784f
m-a <m-a@046ba1d7-8659-4f4f-8137-08d775d845e8>
2003-01-15 17:24:06 +0000

    When retrying to open the files, print the textual error rather than just errno.
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/branches/bf-092-database-1@1339 046ba1d7-8659-4f4f-8137-08d775d845e8

--- bogofilter/wordlists.c
+++ bogofilter/wordlists.c
@@ -125,7 +125,7 @@ retry:
 		    }
 		    goto retry;
 		default:
-		    fprintf(stderr, "Can't open %s (%s) - errno is %d\n", list->filename, list->filepath, err);
+		    fprintf(stderr, "Can't open %s (%s): %s\n", list->filename, list->filepath, strerror(err));
 		    exit(2);
 	    } /* switch */
 	} /* db_open */
