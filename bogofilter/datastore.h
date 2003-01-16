f5228fdcf91b6b9dfd2239ca04961f8ffa76f04a
m-a <m-a@046ba1d7-8659-4f4f-8137-08d775d845e8>
2003-01-16 01:16:47 +0000

    Start doxygen documentation for this file.
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/branches/bf-092-database-1@1344 046ba1d7-8659-4f4f-8137-08d775d845e8

--- bogofilter/datastore.h
+++ bogofilter/datastore.h
@@ -1,73 +1,73 @@
-/* $Id$ */
-
-/* 
- API for bogofilter datastore.
- The idea here is to make bogofilter independent of the database system
- used to store words. The interface specified by this file determines
- the entire interaction between bogofilter and the database
- Writing a new database backend merely requires the implementation of the interface
- 
- Authors: Gyepi Sam <gyepi@praxis-sw.com>   2002 - 2003
-          Matthias Andree <matthias.andree@gmx.de> 2003
- 
-*/
+/**
+ * \file datastore.h
+ * API for bogofilter datastore.  The idea here is to make bogofilter
+ * independent of the database system used to store words. The interface
+ * specified by this file determines the entire interaction between
+ * bogofilter and the database. Writing a new database backend merely
+ * requires the implementation of the interface.
+ *
+ * \author Gyepi Sam  <gyepi@praxis-sw.com>
+ * \author Matthias Andree <matthias.andree@gmx.de>
+ * \date 2002-2003
+ * $Id$
+ */
 
 #ifndef DATASTORE_H
 #define DATASTORE_H
 
 #include "wordlists.h"
 
-/*
-Initialize database, open files, etc.
-params: char * path to database file, char * name of database
-returns: opaque pointer to database handle, which must be saved and
-         passed as the first parameter in all subsequent database function calls. 
-*/
 typedef struct {
     long count;
     long date;
 } dbv_t;
 
-void *db_open(const char *, const char *, dbmode_t, const char *);
+/** Initialize database, open and lock files, etc.
+ * params: char * path to database file, char * name of database
+ * \return opaque pointer to database handle, which must be saved and
+ * passed as the first parameter in all subsequent database function calls. 
+ */
+void *db_open(const char *path /** path to database file */, const char *name /** name of data base */,
+	dbmode_t mode /** open mode, DB_READ or DB_WRITE */, const char *dir /** directory, currently unused */);
 
-/* Close files and clean up. */
+/** Close files and clean up. */
 void  db_close(void *);
 
-/* Flush pending writes to disk */
+/** Flush pending writes to disk */
 void db_flush(void *);
 
-/*
-Increments count for given word.
-Note: negative results are set to zero, 
-*/
+/** Increments count for given word.  Note: negative results are set to
+ * zero.
+ */
 void db_increment(void *, const char *, long);
 
-/*
-Decrement count for a given word, if it exists in the datastore.
-Note: negative results are set to zero, 
-*/
+/** Decrement count for a given word, if it exists in the datastore.
+ * Note: negative results are set to zero. 
+ */
 void db_decrement(void *, const char *, long);
 
-/*
-Retrieve the value associated with a given word in a list
-Returns zero if the word does not exist in the database.
-*/
+/** Retrieve the value associated with a given word in a list. 
+ * \return zero if the word does not exist in the database. 
+ */
 long db_getvalue(void *, const char *);
 
-/* Delete the key */
+/** Delete the key */
 void db_delete(void *, const char *);
 
-/* Set the value associated with a given word in a list */
+/** Set the value associated with a given word in a list */
 void db_setvalue(void *, const char *, long);
 
-/* Get the database message count */
+/** Get the database message count */
 long db_get_msgcount(void*);
 
-/* set the database message count */
+/** set the database message count */
 void db_set_msgcount(void*, long);
 
 typedef int (*db_foreach_t)(char *key, long key_size,
 	char *value, long key_value, void *userdata);
+/** Iterate over all elements in data base and call \p hook for each item.
+ * \p userdata is passed through to the hook function unaltered.
+ */
 int db_foreach(void *, db_foreach_t hook, void *userdata);
 
 #endif
