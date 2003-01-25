0dce339d880f2857d4c1f944be5fa69dd1c76036
relson <relson@046ba1d7-8659-4f4f-8137-08d775d845e8>
2003-01-25 13:32:32 +0000

    Don't decode 'From' lines.
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/trunk/bogofilter@1458 046ba1d7-8659-4f4f-8137-08d775d845e8

--- lexer.c
+++ lexer.c
@@ -152,9 +152,10 @@ int get_decoded_line(byte *buf, size_t max_size)
 	fprintf(stderr, "\n");
     }
 
-    if ((count > 0) && !msg_header && !msg_state->mime_header) {
+    if (count > 0
+	&& memcmp("From ", buf, 5) != 0
+	&& !msg_header && !msg_state->mime_header) {
 	int decoded_count = mime_decode(buf, count);
-
 	/*change buffer size only if the decoding worked */
 	if (decoded_count != 0)
 	    count = decoded_count;
