793e00226bd8ceb5aa68af28c27c3b7b7e2fbdf8
relson <relson@046ba1d7-8659-4f4f-8137-08d775d845e8>
2003-01-25 01:14:12 +0000

    Add debug statements to allow displaying message number when registering a mailbox.
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/trunk@1453 046ba1d7-8659-4f4f-8137-08d775d845e8

--- bogofilter/debug.h
+++ bogofilter/debug.h
@@ -6,13 +6,14 @@
 #define	DEBUG_NONE	0
 
 #ifndef	NODEBUG
-#define BIT_NAMES	"cdfglmstw"
+#define BIT_NAMES	"cdfglmrstw"
 #define BIT_CONFIG	( 1 << ('C' - 'A'))
 #define BIT_DATABASE	( 1 << ('D' - 'A'))
 #define BIT_FORMAT	( 1 << ('F' - 'A'))
 #define BIT_GENERAL	( 1 << ('G' - 'A'))
 #define BIT_LEXER	( 1 << ('L' - 'A'))
 #define BIT_MIME	( 1 << ('M' - 'A'))
+#define BIT_REGISTER	( 1 << ('R' - 'A'))
 #define BIT_SPAMICITY	( 1 << ('S' - 'A'))
 #define BIT_TEXT	( 1 << ('T' - 'A'))
 #define BIT_WORDLIST	( 1 << ('W' - 'A'))
@@ -28,6 +29,7 @@ extern int debug_mask;
 #define DEBUG_FORMAT(level)	0
 #define DEBUG_LEXER(level)	0
 #define DEBUG_MIME(level)	0
+#define DEBUG_REGISTER(level)	0
 #define DEBUG_SPAMICITY(level)	0
 #define DEBUG_TEXT(level)	0
 #define DEBUG_WORDLIST(level)	0
@@ -38,6 +40,7 @@ extern int debug_mask;
 #define DEBUG_FORMAT(level)	((debug_mask & BIT_FORMAT)    && (verbose > level))
 #define DEBUG_LEXER(level)	((debug_mask & BIT_LEXER)     && (verbose > level))
 #define DEBUG_MIME(level)	((debug_mask & BIT_MIME)      && (verbose > level))
+#define DEBUG_REGISTER(level)	((debug_mask & BIT_REGISTER) && (verbose > level))
 #define DEBUG_SPAMICITY(level)	((debug_mask & BIT_SPAMICITY) && (verbose > level))
 #define DEBUG_TEXT(level)	((debug_mask & BIT_TEXT)      && (verbose > level))
 #define DEBUG_WORDLIST(level)	((debug_mask & BIT_WORDLIST)  && (verbose > level))
