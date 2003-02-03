5fb9d7963af584ffbf21d01673fe60ef2af4e2b1
relson <relson@046ba1d7-8659-4f4f-8137-08d775d845e8>
2003-02-03 18:19:45 +0000

    Add script autogen.sh
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/trunk@1586 046ba1d7-8659-4f4f-8137-08d775d845e8

---
+++ bogofilter/autogen.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+# Run this to generate all the initial makefiles, etc.
+
+DIE=0
+
+PROGNAME="bogofilter"
+(autoconf --version) < /dev/null > /dev/null 2>&1 || {
+  echo
+  echo "**Error**: You must have \`autoconf' installed to compile $PROGNAME."
+  echo "Download the appropriate package for your distribution,"
+  echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
+  DIE=1
+}
+
+(automake --version) < /dev/null > /dev/null 2>&1 || {
+  echo
+  echo "**Error**: You must have \`autoconf' installed to compile $PROGNAME."
+  echo "Download the appropriate package for your distribution,"
+  echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
+  DIE=1
+}
+
+if [ "$DIE" -eq 1 ]; then
+  exit 1
+fi
+
+if [ -z "$*" ]; then
+  echo "**Warning**:  Running \`configure' with no arguments."
+  echo "If you wish to pass any to it, specify them on the"
+  echo \`$0\'" command line."
+  echo
+fi
+
+echo "Running 'autoreconf -fis'"
+autoreconf -fis
+echo ""
+
+echo "Running './configure --quiet $*'"
+./configure --quiet $*
+echo ""
+
+echo "Running 'make -s check'"
+make -s check
+echo ""
