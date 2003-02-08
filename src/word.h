358a53324f746968866c23e3479a5f236720fe4e
relson <relson@046ba1d7-8659-4f4f-8137-08d775d845e8>
2003-02-08 20:05:29 +0000

    Add word_cpy() function.
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/trunk/bogofilter@1610 046ba1d7-8659-4f4f-8137-08d775d845e8

--- src/word.h
+++ src/word.h
@@ -24,6 +24,7 @@ typedef struct {
 extern word_t  *word_new(const byte *text, size_t leng);
 extern void 	word_free(word_t *self);
 extern word_t  *word_dup(const word_t *self);
+extern word_t  *word_cpy(word_t *dst, const word_t *src);
 extern int 	word_cmp(const word_t *w1, const word_t *w2);
 extern void 	word_puts(const word_t *self, FILE *fp);
 
