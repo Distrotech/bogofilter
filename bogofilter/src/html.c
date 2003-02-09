e0561a9ffae5f070e96803b9d676760a110cb14b
relson <relson@046ba1d7-8659-4f4f-8137-08d775d845e8>
2003-02-09 22:44:02 +0000

    Cleanup code for killing html comments and fix handling of very long lines, i.e. those exceeding buffer size.
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/trunk@1630 046ba1d7-8659-4f4f-8137-08d775d845e8

--- bogofilter/src/html.c
+++ bogofilter/src/html.c
@@ -10,7 +10,11 @@ AUTHOR:
 
 ******************************************************************************/
 
+#include <ctype.h>
+#include <stdlib.h>
+
 #include "common.h"
+
 #include "html.h"
 #include "lexer.h"
 
@@ -34,22 +38,40 @@ bool	score_html_comments = false;	/* config file option:  "score_html_comments" 
 
 static int kill_html_comment(buff_t *buff);
 
+/* http://www.w3.org/MarkUp/html-spec/html-spec_3.html#SEC3.2.5
+**
+** Comments:
+**
+** To include comments in an HTML document, use a comment declaration.  A comment
+** declaration consists of `<!' followed by zero or more comments followed by `>'. 
+** Each comment starts with `--' and includes all text up to and including the 
+** next occurrence of `--'. In a comment declaration, white space is allowed after 
+** each comment, but not before the first comment. The entire comment declaration 
+** is ignored.
+*/
+
 /* Function Definitions */
 
 int process_html_comments(buff_t *buff)
 {
     byte *tmp;
-    int count;
-    byte *buf = buff->t.text;
-    byte *buf_used = buf + buff->t.leng;
+    byte *buf_start = buff->t.text;
+    byte *buf_used = buf_start + buff->t.leng;
 
-    for (tmp = buf; tmp < buf_used && (tmp = memchr(tmp, '<', buf_used-tmp)) != NULL; tmp += 1) {
-	buff_shrink(buff, tmp - buf);
+    for (tmp = buf_start; tmp < buf_used && (tmp = memchr(tmp, '<', buf_used-tmp)) != NULL; tmp += 1) {
+	int count;
+	buff_shrink(buff, tmp - buf_start);
 	count = kill_html_comment(buff);
 	buf_used = tmp + count;
-	buff_expand(buff, tmp - buf);
+	buff_expand(buff, tmp - buf_start);
     }
-    return buf_used - buf;
+
+    if (buff->t.leng != (size_t)(buf_used - buf_start)) {
+	fprintf(dbgout, "incorrect count:  %d != %d\n", buff->t.leng, (size_t)(buf_used - buf_start));
+	exit(2);
+    }
+
+    return buf_used - buf_start;
 }
 
 static int kill_html_comment(buff_t *buff)
@@ -57,6 +79,7 @@ static int kill_html_comment(buff_t *buff)
     byte *buf_start = buff->t.text;
     byte *buf_used  = buf_start + buff->t.leng;
     byte *buf_end   = buf_start + buff->size;
+    byte *comment = NULL;
 
     int level = 0;
     bool done = false;
@@ -67,13 +90,17 @@ static int kill_html_comment(buff_t *buff)
 	size_t need;
 	size_t avail;
 
-	if (test) {
+/*
+	if (test & 1) {
 	    size_t cspn = strcspn((const char *)tmp, "<>");
 	    tmp += cspn;
-	} 
+	}
+*/
 	c = *tmp;
 
 	need = (c == '<') ? COMMENT_START_LEN : 1;
+
+	buf_used = buf_start + buff->t.leng;
 	avail = buf_used - tmp;
 
 	if (need > avail) {
@@ -84,7 +111,6 @@ static int kill_html_comment(buff_t *buff)
 	    buff_expand(buff, used);
 	    if (new == 0 || new == EOF)
 		break;
-	    buf_used += new;
 	    continue;
 	}
 
@@ -97,6 +123,7 @@ static int kill_html_comment(buff_t *buff)
 	    if (memcmp(tmp, COMMENT_START, COMMENT_START_LEN) != 0)
 		tmp += 1;
 	    else {
+		comment = tmp;
 		if (level == 0 && score_html_comments)
 		    html_comment_count = min(count_html_comments, html_comment_count + 1);
 		level += 1;
@@ -108,18 +135,23 @@ static int kill_html_comment(buff_t *buff)
 	{
 	    /* Hack to only check for ">" rather than complete terminator "-->" */
 	    bool short_check = true;
-	    if (short_check || memcmp(tmp+1-COMMENT_END_LEN, COMMENT_END, COMMENT_END_LEN) == 0) {
+	    if (level == 0)
+		done = true;
+	    if (level > 0 && (short_check || 
+			      memcmp(tmp+1-COMMENT_END_LEN, COMMENT_END, COMMENT_END_LEN) == 0))
+	    {
+		size_t len = short_check ? 1 : COMMENT_END_LEN;
+		tmp += len;
 		/* eat comment */
-		size_t cnt = buf_used - tmp;
 		if (kill_html_comments) {
-		    memmove(buf_start, tmp+1, cnt + 1);
-		    buf_used -= tmp + 1 - buf_start;
-		    buff->read = 0;
-		    buff->t.leng = buf_used - buf_start;
+		    size_t shift = tmp - comment;
+		    buff_shift(buff, comment, shift);
+		    tmp = comment;
 		}
-		tmp = buf_start;
-		if (level > 0)
-		    level -= 1;
+		level -= 1;
+		/* If not followed by a comment, there is no need to keep reading */
+		if (level == 0 && isalnum(*tmp))
+		    done = true;
 	    }
 	    break;
 	}
@@ -128,17 +160,23 @@ static int kill_html_comment(buff_t *buff)
 	    done = level == 0;
 	}
 
+	/* The NULL check for comment is needed when handling lines 
+	** longer than the buffer size.  Compare files msg.line.len.08145
+	** (max len of 8145) and msg.line.len.11269 (max len of 11269)
+	*/
+
 	/* When killing html comments, there's no need to keep it in memory */
-	if (kill_html_comments && buf_end - buf_used < COMMENT_END_LEN) {
+	if (kill_html_comments && 
+	    comment != NULL && 
+	    buf_end - buf_used < COMMENT_END_LEN) 
+	{
 	    /* Leave enough to recognize the end of comment string. */
-	    byte *buf_temp = buf_used - COMMENT_END_LEN;
-	    size_t count = buf_temp - buf_start;
-	    memcpy(buf_start, buf_temp, buf_used - buf_temp);
-	    tmp -= count;
-	    buff->read = 0;
-	    buf_used -= count;
+	    size_t shift = tmp - comment;
+	    buff_shift(buff, comment, shift);
+	    tmp = comment;
+	    buf_used -= shift;
 	}
     }
 
-    return buf_used - buf_start;
+    return buff->t.leng;
 }
