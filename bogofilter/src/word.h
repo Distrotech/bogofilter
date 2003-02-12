f8615b94a06b0f59d5d40744c3afcb6c8fe219a1
relson <relson@046ba1d7-8659-4f4f-8137-08d775d845e8>
2003-02-12 17:34:16 +0000

    Add word_concat() function for use by toknizer.
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/trunk@1648 046ba1d7-8659-4f4f-8137-08d775d845e8

--- bogofilter/src/word.h
+++ bogofilter/src/word.h
@@ -26,6 +26,7 @@ extern void 	word_free(word_t *self);
 extern word_t  *word_dup(const word_t *self);
 extern word_t  *word_cpy(word_t *dst, const word_t *src);
 extern int 	word_cmp(const word_t *w1, const word_t *w2);
+extern word_t  *word_concat(const word_t *w1, const word_t *w2);
 extern void 	word_puts(const word_t *self, size_t width, FILE *fp);
 
 #endif	/* WORD_H */
