0805020a45af3bc404e6666514aeb7074ec7dd2a
relson <relson@046ba1d7-8659-4f4f-8137-08d775d845e8>
2003-02-12 17:48:19 +0000

    Rename function got_newline() to got_emptyline().
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/trunk@1651 046ba1d7-8659-4f4f-8137-08d775d845e8

--- bogofilter/src/token.h
+++ bogofilter/src/token.h
@@ -17,7 +17,7 @@ extern word_t *yylval;
 extern token_t get_token(void);
 
 extern void got_from(void);
-extern void got_newline(void);
+extern void got_emptyline(void);
 
 /* used by lexer_text_html.l */
 extern void html_tag(int level);
