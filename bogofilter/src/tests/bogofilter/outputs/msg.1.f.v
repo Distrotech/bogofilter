6db98b62c7b80f41f17f39ecd2cbc728210b2824
relson <relson@046ba1d7-8659-4f4f-8137-08d775d845e8>
2003-02-24 22:46:30 +0000

    Update reference results since correcting mime boundary pattern changed token counts.
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/trunk@1713 046ba1d7-8659-4f4f-8137-08d775d845e8

--- bogofilter/src/tests/bogofilter/outputs/msg.1.f.v
+++ bogofilter/src/tests/bogofilter/outputs/msg.1.f.v
@@ -1,1 +1,1 @@
-X-Bogosity: Unsure, tests=bogofilter, spamicity=0.478052
+X-Bogosity: Unsure, tests=bogofilter, spamicity=0.470915
--- bogofilter/src/tests/bogofilter/outputs/msg.1.r.v
+++ bogofilter/src/tests/bogofilter/outputs/msg.1.r.v
@@ -1,1 +1,1 @@
-X-Bogosity: No, tests=bogofilter, spamicity=0.388370
+X-Bogosity: No, tests=bogofilter, spamicity=0.386108
