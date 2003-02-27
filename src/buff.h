fe495e5ed816524a667500f45dd97f4fc3e38b96
relson <relson@046ba1d7-8659-4f4f-8137-08d775d845e8>
2003-02-27 02:58:11 +0000

    Extend OO paradigm by further use of name 'self'.
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/trunk/bogofilter@1735 046ba1d7-8659-4f4f-8137-08d775d845e8

--- src/buff.h
+++ src/buff.h
@@ -25,15 +25,15 @@ typedef struct {
 } buff_t;
 
 extern buff_t  *buff_new(byte *buff, size_t used, size_t size);
-extern buff_t  *buff_shrink(buff_t *t, int offset);
-extern buff_t  *buff_expand(buff_t *t, int offset);
-extern void 	buff_free(buff_t *t);
-extern buff_t  *buff_dup(const buff_t *t);
+extern buff_t  *buff_shrink(buff_t *self, int offset);
+extern buff_t  *buff_expand(buff_t *self, int offset);
+extern void 	buff_free(buff_t *self);
+extern buff_t  *buff_dup(const buff_t *self);
 extern int 	buff_cmp(const buff_t *t1, const buff_t *t2);
-extern int	buff_fgetsl(buff_t *, FILE *);
-extern void 	buff_puts(const buff_t *t, size_t width, FILE *fp);
+extern int	buff_fgetsl(buff_t *self, FILE *);
+extern void 	buff_puts(const buff_t *self, size_t width, FILE *fp);
 
-extern void 	buff_shift(buff_t *t, byte *start, size_t length);
+extern void 	buff_shift(buff_t *self, byte *start, size_t length);
 
 #endif	/* BUFF_H */
 
