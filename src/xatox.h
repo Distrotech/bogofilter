ef6d5f62d2d8087f19c9f4e781f4ba07ce379f26
relson <relson@046ba1d7-8659-4f4f-8137-08d775d845e8>
2003-02-27 18:00:23 +0000

    Allow trailing whitespace and comments.
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/trunk/bogofilter@1759 046ba1d7-8659-4f4f-8137-08d775d845e8

--- src/xatox.h
+++ src/xatox.h
@@ -1,3 +1,5 @@
+/* $Id$ */
+
 /** \file xatox.h
  * Header file for xato* string-to-number conversion
  * functions with simplified error checking.
@@ -16,7 +18,7 @@
  * - 1 - success
  */
 int xatoi(int *d /** the result is stored here */ /*@out@*/,
-	const char *in /** input string */);
+	  const char *in /** input string */);
 
 /** strtod variant with simplified error checking. The function tries to parse
  * the floating point string \a in into \a d using the \b strtod(3) ANSI
@@ -24,7 +26,7 @@ int xatoi(int *d /** the result is stored here */ /*@out@*/,
  * - 0 - failure, out of range or illegal characters
  * - 1 - success
  */
-int xatof(double *d /** the result is stored here */ /*@out@*/, const
-	char *in /** input string */);
+int xatof(double *d /** the result is stored here */ /*@out@*/, 
+	  const	char *in /** input string */);
 
 #endif
