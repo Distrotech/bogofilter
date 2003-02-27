7c0880159ee50ffe197a336003d27d95f9fc62c9
m-a <m-a@046ba1d7-8659-4f4f-8137-08d775d845e8>
2003-02-27 00:58:24 +0000

    Revert [x]fgetsl to original interface, add a new buff_fgetsl interface.
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/trunk@1733 046ba1d7-8659-4f4f-8137-08d775d845e8

---
+++ bogofilter/src/buff_fgetsl.h
@@ -0,0 +1,9 @@
+#ifndef BUFF_XFGETSL_H
+#define BUFF_XFGETSL_H
+
+#include <stdio.h>
+#include "buff.h"
+
+int buff_fgetsl(buff_t *, FILE *);
+
+#endif
--- bogofilter/src/fgetsl.h
+++ bogofilter/src/fgetsl.h
@@ -1,19 +1,42 @@
-/* $Id$ */
-
-/*****************************************************************************
-
-NAME:
-   fgetsl.h -- prototype for fgetsl.c
-
-******************************************************************************/
+/**
+ * \file fgetsl.h Header file for NUL-proof fgets() replacement.
+ * \author Matthias Andree
+ * \date 2002
+ * \date 2003
+ */
 
 #ifndef FGETSL_H
 #define FGETSL_H
 
 #include <stdio.h>
-#include "buff.h"
 
-extern int fgetsl(buff_t *buff, FILE *fp);
-extern int xfgetsl(buff_t *buff, FILE *fp, int no_NUL_terminate);
+/** This function reads up to \a size-1 characters from \a stream into \a buf
+ * and adds a terminating NUL character. When the buffer cannot hold at
+ * least one character of payload, the program is aborted.
+ * \return
+ * - negative: EOF or error condition (check ferror(3))
+ * - zero or positive: number of characters read (not counting the
+ *   trailing NUL)
+ */
+extern int fgetsl(char *buf /*@out@*/ /** output buffer */,
+	int size /** capacity of buffer */,
+	FILE *stream /** input stream */);
+
+/** This function reads up to \p size or \p size-1 (depending on \p
+ * no_NUL_terminate) characters from \p stream into \p buf and
+ * optionally adds a terminating NUL character. When the buffer
+ * cannot hold at least one character of payload, the program is
+ * aborted.
+ * \return
+ * - negative: EOF or error condition (check ferror(3))
+ * - zero or positive: number of characters read (not counting the
+ *   trailing NUL)
+ */
+
+extern int xfgetsl(char *buf /*@out@*/ /** output buffer */,
+	int size /** capacity of buffer */,
+	FILE *stream /** input stream */,
+	int no_NUL_terminate /** - if 0, the maximum amount of bytes read is size-1 and the buffer is NUL terminated.
+				 - if non-0, the maximum amount of bytes read is size and the buffer WILL NOT BE NUL terminated. */);
 
 #endif
