7892a0f2acf7e11ba2b39f9382266a10f67db59c
relson <relson@046ba1d7-8659-4f4f-8137-08d775d845e8>
2003-03-01 16:27:54 +0000

    Define HAM_CUTOFF properly for two-state fisher config.
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/trunk/bogofilter@1785 046ba1d7-8659-4f4f-8137-08d775d845e8

--- src/fisher.c
+++ src/fisher.c
@@ -20,9 +20,9 @@ NAME:
 #define	RF_DEBUG
 #undef	RF_DEBUG
 
-#define FISHER_HAM_CUTOFF	0.10f
-#define FISHER_SPAM_CUTOFF	0.95f
-#define FISHER_MIN_DEV		0.10f
+#define FISHER_HAM_CUTOFF	0.00	/* 0.00 for two-state, 0.10 for three-state */
+#define FISHER_SPAM_CUTOFF	0.95
+#define FISHER_MIN_DEV		0.10
 
 /* Function Prototypes */
 
