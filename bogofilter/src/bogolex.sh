547187d2e65dade924b735042f79ec7cd0f45f6f
relson <relson@046ba1d7-8659-4f4f-8137-08d775d845e8>
2003-04-24 04:36:58 +0000

    File also exists in 'tuning'; remove from 'src'.
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/trunk@2152 046ba1d7-8659-4f4f-8137-08d775d845e8

--- bogofilter/src/bogolex.sh
+++
@@ -1,7 +0,0 @@
-if [ -z "$BOGOLEXER"  -o -z "$BOGOUTIL" ] ; then
-    echo BOGOLEXER or BOGOUTIL not set
-else
-( echo .MSG_COUNT ; $BOGOLEXER -p $* | sort -u ) | \
-    $BOGOUTIL -w $BOGOFILTER_DIR | \
-    awk 'NF == 3 { printf( "\"%s\" %s %s\n", $1, $2, $3 ) } '
-fi
