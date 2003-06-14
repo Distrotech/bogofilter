77bb135bce690633662a30fd93b80fe6eaed4858
relson <relson@046ba1d7-8659-4f4f-8137-08d775d845e8>
2003-06-14 22:56:52 +0000

    To allow faster processing of msg-count files, provide simpler and faster routines for reading them.
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/trunk/bogofilter@2449 046ba1d7-8659-4f4f-8137-08d775d845e8

--- src/msgcounts.h
+++ src/msgcounts.h
@@ -13,8 +13,15 @@ AUTHOR:
 #ifndef MSGCOUNTS_H
 #define MSGCOUNTS_H
 
+#include "lexer.h"
+
 extern	long msgs_good;
 extern 	long msgs_bad;
+
+#define	MSG_COUNT_MAX_LEN 100
+extern	yylex_t	 msg_count_lex;
+extern	char	*msg_count_text_ptr;
+extern	size_t	 msg_count_leng;
 
 void init_msg_counts(void);
 void compute_msg_counts(void);
