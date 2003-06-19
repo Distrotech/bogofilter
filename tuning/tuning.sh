c61d4b862156c249c70b8c07b45ade223f4247f4
relson <relson@046ba1d7-8659-4f4f-8137-08d775d845e8>
2003-06-19 01:44:57 +0000

    Multiple updates and corrections:     Include 1 as robs to be tested.     Change $target from environment variable to ARGV for perl code.     Display actual $fpos value, rather than value/3-1.     Exclude from "Top 10 Results" any results where fpos != $target.
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/trunk/bogofilter@2471 046ba1d7-8659-4f4f-8137-08d775d845e8

--- tuning/tuning.sh
+++ tuning/tuning.sh
@@ -11,14 +11,13 @@
 
 # range of values for testing
 
-svals="3.2e-1 1e-1 3.2e-2 1e-2"
+svals="1 3.2e-1 1e-1 3.2e-2 1e-2"
 mvals=`seq 0.025 0.025 0.45001`		# wide value range 
 #mvals=`seq 0.420 0.020 0.46001` 	# high value range
 
 # approx 0.1%-0.3% of nonspam corpus size for selecting spam_cutoff value
 
 target=12
-export target
 
 # file names
 
@@ -70,15 +69,15 @@ if [ ! -f $BOGOFILTER_DIR/goodlist.db -o ! -f $BOGOFILTER_DIR/spamlist.db ] ; th
 fi
 
 function getco () {
-    opts="-m$1 -o$2"
-    shift ; shift
+    opts="-m$1 -o$2"; tgt=$3
+    shift ; shift ; shift
     res=`cat $* | bogofilter -t -c $CFG $opts -v 2>&1 | \
-    perl -e ' $target = $ENV{"target"}; while (<>) { ' \
+    perl -e ' $target = shift @ARGV; while (<>) { ' \
 	 -e ' ($i, $d) = split; push @diffs, $d unless $i != 1; }' \
 	 -e ' die "dainbramage" unless scalar @diffs > 15;' \
          -e ' @s = sort { $a <=> $b } (@diffs); $co = $s[$target];' \
 	 -e ' while($co < 0.000001) { ++$target; $co = $s[$target]; }' \
-	 -e ' printf("%8.6f %d",1.0-$s[$target],$target-1);'`
+	 -e ' printf("%8.6f %d",1.0-$s[$target],$target);' $tgt`
 }
 
 function wrapper () {
@@ -94,17 +93,18 @@ function doit () {
     date=`date "+%m/%d %H:%M:%S"`
     echo -n $date "  "
     printf "%-7s %5.3f fpos..." $rs $md
-    getco $md,$rs 0.10 r0.ns.mc r1.ns.mc r2.ns.mc
-    fpos=${res##* }; co=${res%% *}; let fpos=$fpos/3
+    getco $md,$rs 0.10 $target r0.ns.mc r1.ns.mc r2.ns.mc
+    fpos=${res##* }; co=${res%% *}; 
     printf "%d at cutoff %8.6f, run0..." $fpos $co
+    let fpos3=$fpos/3
     run=0; wrapper $md,$rs $co r0.sp.mc; fneg1=$res
-    echo "$rs $md $co $run $fpos $fneg1" >> $PARM_TBL
+    echo "$rs $md $co $run $fpos3 $fneg1" >> $PARM_TBL
     printf "%3d  run1..." $fneg1
     run=1; wrapper $md,$rs $co r1.sp.mc; fneg2=$res
-    echo "$rs $md $co $run $fpos $fneg2" >> $PARM_TBL
+    echo "$rs $md $co $run $fpos3 $fneg2" >> $PARM_TBL
     printf "%3d  run2..." $fneg2
     run=2; wrapper $md,$rs $co r2.sp.mc; fneg3=$res
-    echo "$rs $md $co $run $fpos $fneg3" >> $PARM_TBL
+    echo "$rs $md $co $run $fpos3 $fneg3" >> $PARM_TBL
     printf "%3d"  $fneg3
     let fneg="$fneg1+$fneg2+$fneg3"
     printf " %4d\n"  $fneg
@@ -125,10 +125,10 @@ done
 
 # get 10 best results (lowest false negative count)
 (echo "" ; \
-echo "Top 10 results:" ; \
+echo "Top 10 Results:" ; \
 echo " robs   min_dev spam_cutoff  run0 run1 run2 total" ; \
-grep fpos < $RESULTS | grep -v cutoff.0.415000 | sed "s@\.\.\.@.. @g" | \
-awk '{printf "%6.4f %7s %12s  %4d %4d %4d %5d\n", $3, $4, $9, $11, $13, $15, $16 }' | \
-tr -d "," | sort -g --key=7 | head -10 ) | tee -a $RESULTS
+cat $RESULTS | grep fpos...$target | grep -v cutoff.0.415000 | sed "s@\.\.\.@.. @g" | \
+awk '{printf "%6.4f %8.3f %10.6f  %4d %4d %4d %5d\n", $3, $4, $9, $11, $13, $15, $16 }' | \
+sort -g --key=7 | head -10 ) | tee -a $RESULTS
 
 date "+%m/%d %H:%M:%S"
