81a556aeed28963e36d0663a6da3b2e161dcd36c
relson <relson@046ba1d7-8659-4f4f-8137-08d775d845e8>
2003-07-18 19:29:44 +0000

    Add standard header.
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/trunk/bogofilter@2575 046ba1d7-8659-4f4f-8137-08d775d845e8

--- src/db_handle_props.h
+++ src/db_handle_props.h
@@ -1,5 +1,11 @@
+/* $Id$ */
 
-/* This file contains fragments of code common to datastore_t?db.c */
+/*****************************************************************************
+
+NAME:
+   db_handle_props.h -- fragments of code common to datastore_t?db.c
+
+******************************************************************************/
 
 /* Get the current process id */
 unsigned long db_handle_pid(void *vhandle){
