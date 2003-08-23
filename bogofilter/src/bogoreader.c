2a63295753f019376b2183165fd118039e2bf2e6
m-a <m-a@046ba1d7-8659-4f4f-8137-08d775d845e8>
2003-08-23 22:53:28 +0000

    Snapshot working maildir reader code. Fixes struct dirent.d_type induced portability problems (broke compile on Solaris).
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/trunk@2869 046ba1d7-8659-4f4f-8137-08d775d845e8

--- bogofilter/src/bogoreader.c
+++ bogofilter/src/bogoreader.c
@@ -5,8 +5,9 @@
 NAME:
    bogoreader.c -- process input files
 
-AUTHOR:
+AUTHORS: (C) Copyright 2003 by
    David Relson <relson@osagesoftware.com>
+   Matthias Andree <matthias.andree@gmx.de>
 
 ******************************************************************************/
 
@@ -14,13 +15,17 @@ AUTHOR:
 #include <dirent.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <errno.h>
 
 #include "buff.h"
 #include "error.h"
 #include "fgetsl.h"
 #include "lexer.h"
 #include "bogoreader.h"
+#include "system.h"
+#include "xmalloc.h"
 
+static void (*fini)(void);
 static int  argc;
 static char **argv;
 static char *filename;
@@ -48,6 +53,8 @@ static lexer_more_t maildir_more;
 static lexer_line_t mailbox_getline;
 static lexer_file_t mailbox_filename;
 
+/* maildir is the mailbox format specified in
+ * http://cr.yp.to/proto/maildir.html */
 static lexer_line_t maildir_getline;
 static lexer_file_t maildir_filename;
 
@@ -56,17 +63,54 @@ static void maildir_fini(void);
 
 /* Function Definitions */
 
-static bool isdir(const char *name)
+/* Checks if name is a directory.
+ * Returns 1 for directory, 0 for other type, -1 for error */
+static int isdir(const char *name)
 {
     struct stat stat_buf;
-    stat(name, &stat_buf);
+    if (stat(name, &stat_buf)) return -1;
     return S_ISDIR(stat_buf.st_mode);
 }
+
+static const char* const maildir_subs[]={ "/new", "/cur", NULL };
+static const char *const *maildir_sub;
+static DIR *maildir_dir;
+
+/* MA: Check if the given name points to a Maildir. We don't require the
+ * /tmp directory for simplicity.
+ * This function checks if dir, dir/new and dir/cur are all directories.
+ * Returns 1 for "yes", 0 for "no" or -1 for "error"
+ */
+static int ismaildir(const char *dir) {
+    int r;
+    size_t l;
+    char *x;
+    const char *const *y;
+    const int maxlen = 4;
+
+    r = isdir(dir);
+    if (r != 1) return r;
+    x = xmalloc((l = strlen(dir)) + maxlen /* append */ + 1 /* NUL */);
+    memcpy(x, dir, l);
+    for (y = maildir_subs; *y; y++) {
+	strlcpy(x + l, *y, maxlen + 1);
+	r = isdir(x);
+	if (r != 1) {
+	    free(x);
+	    return r;
+	}
+    }
+    free(x);
+    return 1;
+}
+
+static void dummy_fini(void) { }
 
 void bogoreader_init(int _argc, char **_argv)
 {
     first = true;
     lexer_getline = mailbox_getline;
+    fini = dummy_fini;
 
     switch (bulk_mode) {
     case B_NORMAL:
@@ -91,7 +135,7 @@ void bogoreader_init(int _argc, char **_argv)
 
 void bogoreader_fini(void)
 {
-    /* Is this really a NOP, or is something missing? */
+    fini();
 }
 
 static bool normal_more(void)
@@ -118,17 +162,25 @@ static bool cmdline_more(void)
 	filename = *argv++;
 	if (fpin)
 	    fclose(fpin);
-	if (!isdir(filename)) {
+	if (isdir(filename) == 0) {
+	    if (DEBUG_READER(0))
+		fprintf(dbgout, "%s:%d - assuming %s is a file\n", __FILE__, __LINE__, filename);
 	    fpin = fopen( filename, "r" );
 	    if (fpin == NULL) {
-		fprintf(stderr, "Can't read file '%s'\n", filename);
+		fprintf(stderr, "Can't open file '%s': %s\n", filename,
+			strerror(errno));
 		return false;
 	    }
 	    emptyline = false;
-	}
-	else {
+	} else if (ismaildir(filename) == 1) {
+	    if (DEBUG_READER(0))
+		fprintf(dbgout, "%s:%d - assuming %s is a maildir\n", __FILE__, __LINE__, filename);
 	    maildir_init(filename);
-	    return maildir_more();
+	    lexer_more = maildir_more;
+	    lexer_more();
+	} else {
+	    fprintf(stderr, "Can't identify type of object '%s'\n", filename);
+	    return false;
 	}
     }
     return val;
@@ -149,7 +201,8 @@ static bool stdin_more(void)
 	fclose(fpin);
     fpin = fopen( filename, "r" );
     if (fpin == NULL) {
-	fprintf(stderr, "Can't read file '%s'\n", filename);
+	fprintf(stderr, "Can't open file '%s': %s\n", filename,
+		strerror(errno));
 	return false;
     }
 
@@ -208,30 +261,27 @@ static int mailbox_getline(buff_t *buff)
     return count;
 }
 
-static DIR *dir;
-
-static void maildir_init(const char *name)
+static void maildir_fini(void)
 {
-    dir = opendir(name);
-
-    if (dir == NULL) {
-	perror(name);
-	exit(EX_ERROR);
-    }
-
-    lexer_more = maildir_more;
-    lexer_filename = maildir_filename;
-    lexer_getline  = maildir_getline;
-
-    strlcpy(dirname, name, sizeof(dirname));
-
+    if (maildir_dir)
+	closedir(maildir_dir);
+    if (fpin)
+	fclose(fpin);
+    maildir_dir = NULL;
     return;
 }
 
-static void maildir_fini(void)
+static void maildir_init(const char *name)
 {
-    closedir(dir);
-    dir = NULL;
+    lexer_more = maildir_more;
+    lexer_filename = maildir_filename;
+    lexer_getline  = maildir_getline;
+    maildir_sub = maildir_subs;
+    maildir_dir = NULL;
+    fini = maildir_fini;
+
+    strlcpy(dirname, name, sizeof(dirname));
+
     return;
 }
 
@@ -239,28 +289,57 @@ static bool maildir_more(void)
 {
     struct dirent *dirent;
 
-    while ((dirent = readdir(dir)) != NULL) {
-	if (dirent->d_type != DT_DIR &&
-	    *dirent->d_name != '.')
-	    break;
+trynext: /* ugly but simple */
+    if (maildir_dir == NULL) {
+	/* open next directory */
+	char *x;
+	size_t siz;
+
+	if (*maildir_sub == NULL)
+	    return false; /* IMPORTANT for termination */
+	siz = strlen(dirname) + 4 + 1;
+	x = xmalloc(siz);
+	strlcpy(x, dirname, siz);
+	strlcat(x, *(maildir_sub++), siz);
+	maildir_dir = opendir(x);
+	if (!maildir_dir) {
+	    fprintf(stderr, "cannot open directory '%s': %s", x,
+		    strerror(errno));
+	}
+	free(x);
+    }
+
+    while ((dirent = readdir(maildir_dir)) != NULL) {
+	/* skip dot files */
+	if (dirent->d_name[0] == '.')
+	    continue;
+
+	/* there's no need to skip directories here, we can do that
+	 * later */
+	break;
     }
 
     if (dirent == NULL) {
-	maildir_fini();
-	return false;
+	closedir(maildir_dir);
+	maildir_dir = NULL;
+	goto trynext;
     }
 
     filename = namebuff;
-    snprintf(namebuff, sizeof(namebuff), "%s/%s", dirname, dirent->d_name);
+    snprintf(namebuff, sizeof(namebuff), "%s%s/%s", dirname, *(maildir_sub-1),
+	    dirent->d_name);
 
     if (fpin)
 	fclose(fpin);
     fpin = fopen( filename, "r" );
     if (fpin == NULL) {
-	fprintf(stderr, "Can't read file '%s'\n", filename);
-	return false;
+	fprintf(stderr, "Warning: can't open file '%s': %s\n", filename,
+		strerror(errno));
+	/* don't barf, the file may have been changed by another MUA,
+	 * or a directory that just doesn't belong there, just skip it */
+	goto trynext;
     }
-    
+
     if (DEBUG_READER(0))
 	fprintf(dbgout, "%s:%d - reading %s (%p)\n", __FILE__, __LINE__, filename, fpin);
 
