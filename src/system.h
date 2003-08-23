e5ca4ddccd22b0428242874d3a25b1896d9bfd0e
m-a <m-a@046ba1d7-8659-4f4f-8137-08d775d845e8>
2003-08-23 22:45:02 +0000

    Handle <dirent.h> and predecessors.
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/trunk/bogofilter@2868 046ba1d7-8659-4f4f-8137-08d775d845e8

--- src/system.h
+++ src/system.h
@@ -157,4 +157,23 @@ typedef uint8_t u_int8_t;
 #include <fcntl.h>
 #endif
 
+/* dirent.h surroundings */
+/* from autoconf.info */
+#if HAVE_DIRENT_H
+# include <dirent.h>
+# define NAMLEN(dirent) strlen((dirent)->d_name)
+#else
+# define dirent direct
+# define NAMLEN(dirent) (dirent)->d_namlen
+# if HAVE_SYS_NDIR_H
+#  include <sys/ndir.h>
+# endif
+# if HAVE_SYS_DIR_H
+#  include <sys/dir.h>
+# endif
+# if HAVE_NDIR_H
+#  include <ndir.h>
+# endif
+#endif
+
 #endif /* SYSTEM_H */
