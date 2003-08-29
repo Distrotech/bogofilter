f90b52e04cf2264b09ec6d48175d50110a56ac57
m-a <m-a@046ba1d7-8659-4f4f-8137-08d775d845e8>
2003-08-29 12:05:44 +0000

    Major type cleanup, fixing bogofilter -Q garbage reporting (bug reported by jxz).
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/trunk/bogofilter@2911 046ba1d7-8659-4f4f-8137-08d775d845e8

--- src/format.c
+++ src/format.c
@@ -78,16 +78,15 @@ const char *terse_format = "%1.1c %f";
 const char *log_header_format = "%h: %c, spamicity=%p, version=%v";
 const char *log_update_format = "register-%r, %w words, %m messages";
 
-#define	RC_COUNT RC_UNSURE+1	/* 3 (for Spam/Ham/Unsure) */
-
 typedef const char *FIELD;
-typedef FIELD FIELDS[RC_COUNT];
 
-FIELDS spamicity_tags_ynu = { "Yes",  "No",  "Unsure" };
-FIELDS spamicity_tags_shu = { "Spam", "Ham", "Unsure" };
+#define RC_COUNT 3
+static FIELD spamicity_tags_ynu[] = { "Yes",  "No",  "Unsure" };
+static FIELD spamicity_tags_shu[] = { "Spam", "Ham", "Unsure" };
+static FIELD spamicity_format_d[] = { "%0.6f", "%0.6f", "%0.6f"  };
 
 FIELD  *spamicity_tags    = spamicity_tags_ynu;
-FIELDS spamicity_formats = { "%0.6f", "%0.6f", "%0.6f"  };
+FIELD  *spamicity_formats = spamicity_format_d;
 
 static bool set_spamicity_tags(const unsigned char *val);
 static bool set_spamicity_formats(const unsigned char *val);
@@ -180,10 +179,14 @@ static size_t format_date(char *dest, const char *destend)
     time_t t;
     size_t l;
 
-    time(&t);
+    /* must check, size_t will wrap to INT_MAX or LONG_MAX otherwise */
+    if (dest == destend)
+	return 0;
+
+    (void)time(&t);
     tm = gmtime(&t);
-    l = strftime(dest, destend - dest - 1, "%Y-%m-%dT%H:%M:%SZ", tm);
-    if (l + 1 >= destend - dest) /* sanitize return */
+    l = strftime(dest, (size_t)(destend - dest - 1), "%Y-%m-%dT%H:%M:%SZ", tm);
+    if (l + 1 >= (size_t)(destend - dest)) /* sanitize return */
 	l = 0;
     return l;
 }
@@ -218,7 +221,7 @@ static size_t format_string(char *dest, const char *src, int min, int prec, int 
 {
     size_t s_len = strlen(src);
     int len;
-    if (s_len > INT_MAX) {
+    if (s_len > (size_t)INT_MAX) {
 	fprintf(stderr, "cannot handle string length (%lu) above %d, aborting\n", (unsigned long)s_len, INT_MAX);
 	internal_error;
     }
@@ -241,7 +244,9 @@ static size_t format_string(char *dest, const char *src, int min, int prec, int 
 static size_t format_spamicity(char *dest, const char *fmt, double spamicity, const char *destend)
 {
     char temp[20];
-    size_t len = sprintf(temp, fmt, spamicity);
+    size_t len = snprintf(temp, sizeof(temp), fmt, spamicity);
+    if (len > INT_MAX)
+	len = INT_MAX; /* clamp */
     len = format_string(dest, temp, 0, len, 0, destend);
     return len;
 }
@@ -268,7 +273,8 @@ char *convert_format_to_string(char *buff, size_t size, const char *format)
     char temp[20];
 
     int state = S_DEFAULT;
-    size_t min = 0, prec = 0;
+    int min = 0;
+    int prec = 0;
     int flags = 0;
 
     rc_t status = method->status();
