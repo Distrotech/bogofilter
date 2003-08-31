0b52c73c91a3bda1e10985d7001dede329311364
m-a <m-a@046ba1d7-8659-4f4f-8137-08d775d845e8>
2003-08-31 16:18:21 +0000

    Change version.c, no longer depends on other .c files. Breaks circular dependency. Try to extract date from CVS/Entries files when Perl5 and HTTP::Date module are installed, fall back to current date otherwise.
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/trunk/bogofilter@2957 046ba1d7-8659-4f4f-8137-08d775d845e8

--- src/version.sh
+++ src/version.sh
@@ -5,30 +5,44 @@
 #	create version.h to define BOGOFILTER_VERSION
 #
 #	if VERSION in config.h does not contain 'cvs', use its value.
-#	if it does contain 'cvs', append the a 4 digit date to it.
-#	the 4 digit date is the newer of the two $Id: dates
-#	in main.c and bogofilter.c
+#	if it does contain 'cvs', append a date to it.
 #
-#	const char *const version = "0.9.1"
-# or:
-#	const char *const version = "0.9.1.cvs.1130"
+#	We'll first try to find CVS/Entries files and use the most
+#	current date from the files. To parse, we need Perl 5 and
+#	the HTTP::Date module.
+#
+#	If that files, either because HTTP::Date is missing, Perl is
+#	missing or we're building outside the CVS working directory,
+#	we'll fall back to use the current date (GMT time zone) with
+#	hour precision.
+
+set -e
 
 VERSION=`grep define.VERSION config.h | awk '{print $3}' | tr -d '"'`
 
-SUFFIX=`echo $VERSION | egrep "[a-z][a-z][a-z]$"`
+SUFFIX=`echo $VERSION | egrep "\.cvs$"`
 
 srcdir=$1
 shift
 
 if [ ! -z "$SUFFIX" ]; then
-   DATE="00000000"
-   for file in $* ; do
-       HEAD=`head -1 $srcdir/$file | awk '{print $5}' | tr -d "/"`
-       if [ $HEAD -gt $DATE  ] ; then
-	   DATE="$HEAD"
-       fi
-   done
-   VERSION="$VERSION.$DATE"
+    FILES=`find $srcdir -name CVS -type d | while read a ; do find "$a" -name Entries -type f ; done`
+    set +e
+    DATE=CVStime_`perl -MHTTP::Date -e '
+    $max = 0;
+    while (<>) {
+	split m(/);
+	$a=str2time($_[3], "GMT");
+	$max=$a if $a and $a > $max;
+    }
+    $date=HTTP::Date::time2isoz($max);
+    $date=~tr/ :Z-/_/d;
+    print $date, "\n";
+    ' </dev/null $FILES` || DATE=
+    if [ "x$FILES" = "x" ] || [ "x$DATE" = "x" ] ; then
+       DATE=`env TZ=GMT date "+build_date_%Y%m%d_%Hh"`
+    fi
+    VERSION="$VERSION.$DATE"
 fi
 
 echo "const char * const version = \"$VERSION\";"
