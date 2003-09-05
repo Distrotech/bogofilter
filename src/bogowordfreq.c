70d05b32dd91254f8319e4573b7753d8c9c41727
relson <relson@046ba1d7-8659-4f4f-8137-08d775d845e8>
2003-09-05 22:23:52 +0000

    Update to work with current lexer and bogoreader.
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/trunk/bogofilter@2992 046ba1d7-8659-4f4f-8137-08d775d845e8

--- src/bogowordfreq.c
+++ src/bogowordfreq.c
@@ -1,3 +1,5 @@
+/* $Id$ */
+
 /* bogowordfreq.c -- program to count token frequency of stdin */
 /* (C) 2002 by Matthias Andree
  * redistributable under the terms of the GNU General Public License v2
@@ -16,7 +18,10 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "bogoreader.h"
+#include "charset.h"
 #include "collect.h"
+#include "mime.h"
 #include "wordhash.h"
 #include "wordlists.h"
 
@@ -24,6 +29,8 @@ const char *progname = "bogowordfreq.c";
 const char *spam_header_name = "X-Bogosity:"; /* unused */
 
 wordlist_t* word_lists=NULL;	/* define to satisfy link requirements of msgcount.c */
+
+void initialize(void);
 
 static void print_wordlist (wordhash_t *h)
 {
@@ -39,17 +46,36 @@ static void print_wordlist (wordhash_t *h)
 
 int main(int argc, char **argv)
 {
-    bool more;
+    max_repeats=1;		/* set default value */
 
-    if (argc >= 2) max_repeats=atoi(argv[1]);
-    
-    do {
+    if (argc >= 2) {
+	max_repeats=atoi(argv[1]);
+	argc -= 1;
+	argv += 1;
+    }
+
+    fpin = stdin;
+    dbgout = stdout;
+
+    mbox_mode = true;		/* to allow multiple messages */
+    bogoreader_init(argc, argv);
+
+    while (reader_more()) {
 	wordhash_t *h = wordhash_init();
-	more = collect_words(h);
+	initialize();
+	collect_words(h);
 	printf("%ld tokens:\n", (long) h->wordcount);
 	print_wordlist(h);
 	printf("\n");
 	wordhash_free(h);
-    } while (more);
+    }
     return 0;
+}
+
+void initialize()
+{
+    init_charset_table(charset_default, true);
+    mime_reset();
+    token_init();
+    lexer_v3_init(NULL);
 }
