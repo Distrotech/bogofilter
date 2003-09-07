c2cb58de994be3623518314a760a0da2e69049eb
m-a <m-a@046ba1d7-8659-4f4f-8137-08d775d845e8>
2003-09-07 01:35:49 +0000

    Fix warnings.
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/trunk@3027 046ba1d7-8659-4f4f-8137-08d775d845e8

--- bogofilter/src/datastore_qdbm.c
+++ bogofilter/src/datastore_qdbm.c
@@ -26,6 +26,7 @@ Stefan Bellon <sbellon@sbellon.de> 2003
 #include "datastore_db.h"
 #include "maint.h"
 #include "error.h"
+#include "paths.h"
 #include "xmalloc.h"
 #include "xstrdup.h"
 
@@ -254,7 +255,7 @@ int db_foreach(dsh_t *dsh, db_foreach_t hook, void *userdata)
     datum key, data;
     dbv_t dbv_key, dbv_data;
 
-    int ret;
+    int ret = 0;
 
     key = dbm_firstkey(dbp);
     while (key.dptr) {
