5fc6d8494931a429438169b6f99c96dd51bf360f
m-a <m-a@046ba1d7-8659-4f4f-8137-08d775d845e8>
2003-09-17 13:02:51 +0000

    Fix warnings.
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/trunk@3117 046ba1d7-8659-4f4f-8137-08d775d845e8

--- bogofilter/src/error.c
+++ bogofilter/src/error.c
@@ -25,23 +25,21 @@ AUTHOR:
 void print_error( const char *file, unsigned long line, const char *format, ... )
 {
     char message[256];
-    int i, l;
+    size_t l;
 
     va_list ap;
     va_start(ap, format);
-    l = vsnprintf(message, sizeof(message), format, ap);
+    l = (size_t)vsnprintf(message, sizeof(message), format, ap);
     if (l >= sizeof(message)) {
 	/* output was truncated, mark truncation */
 	strcpy(message + sizeof(message) - 4, "...");
     }
     va_end(ap);
 
-    
-
     /* security: replace unprintable characters by underscore "_" */
-    for (i = 0; i < strlen(message); i++)
-	if (!isprint((unsigned char)message[i]))
-	    message[i] = '_';
+    for (l = 0; l < strlen(message); l++)
+	if (!isprint((unsigned char)message[l]))
+	    message[l] = '_';
 
     fprintf(stderr, "%s: %s\n", progname, message);
 #ifdef HAVE_SYSLOG_H
