09e8fb032e13decf759a768f458bad8bd183e3ae
relson <relson@046ba1d7-8659-4f4f-8137-08d775d845e8>
2003-09-28 13:14:31 +0000

    Revise OS/2 compatibility code.
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/trunk/bogofilter@3207 046ba1d7-8659-4f4f-8137-08d775d845e8

--- src/system.c
+++ src/system.c
@@ -3,19 +3,23 @@
 /*****************************************************************************
 
 NAME:
-   system.c -- bogofilter covers for OS dependent functions.
+   system.c -- bogofilter covers for OS/compiler dependent functions.
 
 ******************************************************************************/
 
 #include "common.h"
+#if defined(__IBMC__) || defined(__IBMCPP__) || defined(__WATCOMC__)
+#define _OS2_
+#include "direct.h"
+#endif
 
 int bf_mkdir(const char *path, mode_t mode)
 {
     int rc;
-#if !defined(__IBMC__) && !defined(__IBMCPP__)
+#ifndef _OS2_
     rc = mkdir(path, mode);
 #else
-    rc = mkdir(path);
+    rc = mkdir((unsigned char*)path);
 #endif
     return rc;
 }
