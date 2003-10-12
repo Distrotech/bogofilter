bd9a6f7b24c2d8f79914b36a737bbd23c2a5bd88
relson <relson@046ba1d7-8659-4f4f-8137-08d775d845e8>
2003-10-12 21:32:24 +0000

    Re-group local variables and add file and line info to memdisplay() output.
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/trunk/bogofilter@3343 046ba1d7-8659-4f4f-8137-08d775d845e8

--- src/memdebug.h
+++ src/memdebug.h
@@ -9,11 +9,16 @@
 #ifndef MEMDEBUG_H
 #define MEMDEBUG_H
 
+#undef	MEMDISPLAY
+#define	MEMDISPLAY	memdisplay(__FILE__, __LINE__)
+
+extern int memtrace;
+
 void *md_calloc(size_t nmemb, size_t size);
 void *md_malloc(size_t size);
 void *md_realloc(void *ptr, size_t size);
 void  md_free(void *ptr);
-void  memdisplay(void);
+void  memdisplay(const char *file, int lineno);
 
 enum { M_MALLOC = 1, 
        M_FREE = 2 
