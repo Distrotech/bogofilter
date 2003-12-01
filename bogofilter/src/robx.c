76b45f8893d664a49d2dd00ea3141a8e1b78b4f7
relson <relson@046ba1d7-8659-4f4f-8137-08d775d845e8>
2003-12-01 22:48:49 +0000

    No need to change globals msgs_good and msgs_bad.
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/trunk@3641 046ba1d7-8659-4f4f-8137-08d775d845e8

--- bogofilter/src/robx.c
+++ bogofilter/src/robx.c
@@ -113,8 +113,8 @@ static double compute_robx(dsh_t *dsh)
     struct robhook_data rh;
 
     ds_get_msgcounts(dsh, &val);
-    msgs_bad  = spam_cnt = val.spamcount;
-    msgs_good = good_cnt = val.goodcount;
+    spam_cnt = val.spamcount;
+    good_cnt = val.goodcount;
 
     rh.scalefactor = (double)spam_cnt/(double)good_cnt;
     rh.dsh = dsh;
