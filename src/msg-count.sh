bde97b08ad2c65821df41fae8b308436d14698ae
relson <relson@046ba1d7-8659-4f4f-8137-08d775d845e8>
2003-12-03 16:32:20 +0000

    Update help message.
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/trunk/bogofilter@3650 046ba1d7-8659-4f4f-8137-08d775d845e8

--- src/msg-count.sh
+++ src/msg-count.sh
@@ -5,13 +5,14 @@
 #	performs lexical analysis and database lookup
 #	and converts an email to the msg-count format
 #
-if [ "x$1" = "x-?" -o "x$1" = "x-h" ]; then
+if [ "x$1" = "x-?" ] || [ "x$1" = "x-h" ]; then
     cat <<EOT
 NAME
   msg-count.sh - lexical analysis and database lookup for an email message
                  to convert it to the msg-count format	
 SYNOPSIS
   msg-count.sh [-h | path/to/bogofilter/directory [bogolexer options]]
+
 DESCRIPTION
   msg-count.sh creates a message digest consisting of a .MSG_COUNT line,
   followed by one line per token. The format is
@@ -23,16 +24,21 @@ DESCRIPTION
   The .MSG_COUNT line gives the numbers of messages that make up the
   spam and nonspam wordlists, respectively; each token line gives the
   counts of the token in the spam and nonspam wordlists, respectively.
-  The bogol script is meant to be invoked by a command similar to
+  The msg-count script is meant to be invoked by commands like:
 
-    formail -s msg-count.sh dbdir <mboxfile >digestfile
+    formail -s msg-count.sh db_dir <mboxfile >digestfile
+
+    msg-count.sh db_dir msg_dir >digestfile
+
 OPTIONS
   -h    The option -h displays help.
+
 BOGOFILTER DIRECTORY
   The bogofilter directory is where the wordlists are kept. It defaults
   to ~/.bogofilter if no path is provided on the command line.  The
   path must be provided if further options are given; any further
   options are passed through to bogolexer.
+
 SEE ALSO
   bogofilter(1), bogolexer(1), bogoutil(1).
 EOT
