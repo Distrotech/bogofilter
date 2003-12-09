49e160bf641f21faa7a037dca6c9a2a265746068
relson <relson@046ba1d7-8659-4f4f-8137-08d775d845e8>
2003-12-09 02:08:45 +0000

    Use 'leng' attribute when printing, not 'size'.
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/trunk/bogofilter@3705 046ba1d7-8659-4f4f-8137-08d775d845e8

--- src/datastore_qdbm.c
+++ src/datastore_qdbm.c
@@ -237,7 +237,7 @@ int db_set_dbvalue(dsh_t *dsh, const dbv_t *token, dbv_t *val)
     if (ret == 0) {
 	print_error(__FILE__, __LINE__,
 		    "(qdbm) db_set_dbvalue( '%.*s' ), err: %d",
-		    CLAMP_INT_MAX(token->size), (char *)token->data, dpecode);
+		    CLAMP_INT_MAX(token->leng), (char *)token->data, dpecode);
 	exit(EX_ERROR);
     }
 
