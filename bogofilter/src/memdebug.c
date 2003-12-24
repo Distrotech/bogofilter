d07a394dbc548f538db20ab56c943f466660a50f
relson <relson@046ba1d7-8659-4f4f-8137-08d775d845e8>
2003-12-24 04:18:59 +0000

    Add trailing fence.
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/trunk@3794 046ba1d7-8659-4f4f-8137-08d775d845e8

--- bogofilter/src/memdebug.c
+++ bogofilter/src/memdebug.c
@@ -33,8 +33,8 @@ int  memtrace= 0 * (M_MALLOC+M_FREE);
 
 uint32_t dbg_trap_index= 0;
 uint32_t dbg_size      = 0;
-uint32_t dbg_index_min = 0;
-uint32_t dbg_index_max = 0;
+uint32_t dbg_index_min = 0;	/* 1 */
+uint32_t dbg_index_max = 0;	/* 1000 */
 uint32_t dbg_size_trap = 0;	/* 100000 */
 
 #define	MB 1000000
@@ -59,10 +59,14 @@ void md_trap(const char *why);
 void md_trap(const char *why) { (void)why; }
 
 typedef struct memheader {
-    size_t	size;
+    uint32_t	size;
     uint32_t	indx;
     uint32_t	tag;
 } mh_t;
+
+typedef struct memtrailer {
+    uint32_t	tag;
+} mt_t;
 
 void mh_disp(const char *s, mh_t *p);
 void mh_disp(const char *s, mh_t *p)
@@ -79,7 +83,8 @@ void mh_disp(const char *s, mh_t *p)
 void *
 md_malloc(size_t size)
 {
-    void *x;
+    void *ptr;
+    mt_t *mt;
     mh_t *mh = NULL;
 
     ++cnt_malloc;
@@ -93,7 +98,7 @@ md_malloc(size_t size)
 
     max_malloc = max(max_malloc, cur_malloc);
     tot_malloc += size;
-    size += sizeof(mh_t);		/* Include size storage */
+    size += sizeof(mh_t) + sizeof(md_tag);		/* Include size storage */
 
     if (dbg_size_trap != 0 && size > dbg_size_trap)
 	md_trap("dbg_size_trap");
@@ -103,10 +108,10 @@ md_malloc(size_t size)
 	exit(EX_ERROR);
     }
 
-    x = malloc(size);
+    ptr = malloc(size);
 
-    mh = (mh_t *) x;
-    mh->size = size - sizeof(mh_t);
+    mh = (mh_t *) ptr;
+    mh->size = size - sizeof(mh_t) - sizeof(md_tag);
     mh->indx = ++cnt_alloc;
     mh->tag  = md_tag;
 
@@ -115,18 +120,25 @@ md_malloc(size_t size)
     if (dbg_trap_index != 0 && mh->indx == dbg_trap_index)
 	md_trap("dbg_index");
 
-    x = (void *) (mh+1);
+    mt = (mt_t *)((char *)(mh+1)+mh->size);
+    mt->tag = md_tag;
 
-    return x;
+    ptr = (void *) (mh+1);
+
+    return ptr;
 }
 
 void
 md_free(void *ptr)
 {
-    mh_t *mh = ((mh_t *) ptr) - 1;
+    mt_t *mt;
+    mh_t *mh = (mh_t *) ptr;
 
     if (!ptr)
 	return;
+
+    mh -= 1;
+    ptr = (void *) mh;
 
     ++cnt_free;
     if (memtrace & M_FREE)
@@ -138,10 +150,13 @@ md_free(void *ptr)
     if (mh->size > cur_malloc || 
 	(dbg_size_trap != 0 && mh->size > dbg_size_trap))
 	md_trap("dbg_size_trap");
+    mt = (mt_t *)((char *)(mh+1)+mh->size);
+    if (mt->tag != md_tag)
+	md_trap("md_tag");
+
     cur_malloc -= mh->size;
 
     mh->tag = -1;
-    ptr = (void *) mh;
 
     free(ptr);
 }
@@ -170,6 +185,7 @@ void
 *md_calloc(size_t nmemb, size_t size)
 {
     void *ptr;
+    mt_t *mt;
     mh_t *mh;
 
     size = size * nmemb;
@@ -177,7 +193,7 @@ void
     cur_malloc += size;
     max_malloc = max(max_malloc, cur_malloc);
     tot_malloc += size;
-    size += sizeof(mh_t);		/* Include size storage */
+    size += sizeof(mh_t) + sizeof(md_tag);		/* Include size storage */
     ++cnt_calloc;
 
     if (dbg_too_much != 0 && max_malloc > dbg_too_much) {
@@ -189,7 +205,8 @@ void
     ptr = calloc(nmemb, size);
 
     mh = (mh_t *) ptr;
-    mh->size = size - sizeof(mh_t);
+
+    mh->size = size - sizeof(mh_t) - sizeof(md_tag);
     mh->indx = ++cnt_alloc;
     mh->tag  = md_tag;
 
@@ -197,6 +214,9 @@ void
 	mh_disp( "c", mh );
     if (dbg_trap_index != 0 && mh->indx == dbg_trap_index)
 	md_trap("dbg_trap_index");
+
+    mt = (mt_t *)((char *)(mh+1)+mh->size);
+    mt->tag = md_tag;
 
     ptr = (void *) (mh+1);
 
@@ -207,6 +227,7 @@ void
 *md_realloc(void *ptr, size_t size)
 {
     mh_t *mh = ((mh_t *) ptr)-1;
+    mt_t *mt;
     size_t oldsize = mh->size;
 
     cur_malloc += size - oldsize;
@@ -223,15 +244,18 @@ void
     if (memtrace & M_FREE)
 	mh_disp( "r", mh );
 
-    size = size + sizeof(mh_t);
+    size = size + sizeof(mh_t) + sizeof(md_tag);
     mh = realloc(mh, size);
 
-    mh->size = size - sizeof(mh_t);
+    mh->size = size - sizeof(mh_t) - sizeof(md_tag);
     mh->indx = ++cnt_realloc;
     mh->tag  = md_tag;
 
     if (memtrace & M_MALLOC)
 	mh_disp( "r", mh );
+
+    mt = (mt_t *)((char *)mh+mh->size);
+    mt->tag = md_tag;
 
     ptr = (void *) (mh+1);
 
