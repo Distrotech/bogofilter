375cea2092993ab687bb7ecd816ab1997bbc6a10
relson <relson@046ba1d7-8659-4f4f-8137-08d775d845e8>
2004-01-03 14:09:03 +0000

    Add histogram capability to bogoutil.
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/trunk/bogofilter@3856 046ba1d7-8659-4f4f-8137-08d775d845e8

---
+++ src/bogohist.h
@@ -0,0 +1,19 @@
+/* $Id$ */
+
+/*****************************************************************************
+
+NAME:
+   histogram.h -- constants and declarations for histogram.c
+
+AUTHOR:
+   David Relson <relson@osagesoftware.com>
+
+******************************************************************************/
+
+#ifndef	HISTOGRAM_H
+#define	HISTOGRAM_H
+
+extern int histogram(const char *path);
+
+#endif	/* HISTOGRAM_H */
+
