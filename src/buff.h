22749320747a41cdc9337a3bdd39ee357f7dd0f0
relson <relson@046ba1d7-8659-4f4f-8137-08d775d845e8>
2004-01-25 20:57:18 +0000

    Use 'uint' where appropriate.
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/trunk/bogofilter@3989 046ba1d7-8659-4f4f-8137-08d775d845e8

--- src/buff.h
+++ src/buff.h
@@ -13,28 +13,26 @@ AUTHOR:
 #ifndef	BUFF_H
 #define	BUFF_H
 
-#include <stdio.h>
-
 #include "word.h"
 
 typedef struct {
-    word_t		t;
-    unsigned int	read;	/* start of last read */
-    unsigned int   size;	/* capacity */
+    word_t t;
+    uint   read;	/* start of last read */
+    uint   size;	/* capacity */
 } buff_t;
 
-extern buff_t  *buff_new(byte *buff, unsigned int used, unsigned int size);
-extern buff_t  *buff_init(buff_t *self, byte *buff, unsigned int used, unsigned int size);
+extern buff_t  *buff_new(byte *buff, uint used, uint size);
+extern buff_t  *buff_init(buff_t *self, byte *buff, uint used, uint size);
 extern void 	buff_free(buff_t *self);
 extern void	buff_free_text(buff_t *self);
 extern int	buff_add(buff_t *self, word_t *in);
 extern buff_t  *buff_dup(const buff_t *self);
 extern int 	buff_cmp(const buff_t *t1, const buff_t *t2);
 extern int	buff_fgetsl(buff_t *self, FILE *);
-extern int	buff_fgetsln(buff_t *self, FILE *, unsigned int);
-extern void 	buff_puts(const buff_t *self, unsigned int width, FILE *fp);
+extern int	buff_fgetsln(buff_t *self, FILE *, uint);
+extern void 	buff_puts(const buff_t *self, uint width, FILE *fp);
 
-extern void 	buff_shift(buff_t *self, byte *start, unsigned int length);
+extern void 	buff_shift(buff_t *self, byte *start, uint length);
 
 #endif	/* BUFF_H */
 
