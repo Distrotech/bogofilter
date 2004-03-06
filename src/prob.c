cf1c811d012764d7a0c88f040c858ea9ed7a4aad
m-a <m-a@046ba1d7-8659-4f4f-8137-08d775d845e8>
2004-03-06 21:40:08 +0000

    Add some documentation and use a form with fewer divisions.
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/trunk/bogofilter@4219 046ba1d7-8659-4f4f-8137-08d775d845e8

--- src/prob.c
+++ src/prob.c
@@ -14,6 +14,8 @@ AUTHORS:
 
 #include "prob.h"
 
+
+/* http://www.linuxjournal.com/article.php?sid=6467 */
 double calc_prob(uint good, uint bad)
 {
     int n = good + bad;
@@ -22,7 +24,15 @@ double calc_prob(uint good, uint bad)
     if (n == 0)
 	fw = robx;
     else {
+	/* the original code had four divisions rather than one:
 	double pw = ((bad / msgs_bad) / (bad / msgs_bad + good / msgs_good));
+	*/
+	double pw = bad*msgs_good / (bad*msgs_good + good*msgs_bad);
+
+	/* robs is Robinson's s parameter, the "strenght of background info" */
+	/* robx is Robinson's x parameter, the assumed probability that
+	 * a word we don't have enough info about will be spam */
+	/* n is the amount of mails that contain the word w */
 	fw = (robs * robx + n * pw) / (robs + n);
     }
 
