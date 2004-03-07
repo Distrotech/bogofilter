86ac8c5825437ec50dd5529ce9a098ee9e76026f
relson <relson@046ba1d7-8659-4f4f-8137-08d775d845e8>
2004-03-07 02:46:20 +0000

    Revise comments, reformat, and update authorship.
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/trunk/bogofilter@4221 046ba1d7-8659-4f4f-8137-08d775d845e8

--- src/prob.c
+++ src/prob.c
@@ -7,6 +7,7 @@ NAME:
 
 AUTHORS:
    David Relson <relson@osagesoftware.com>
+   Matthias Andree <matthias.andree@gmx.de>
 
 ******************************************************************************/
 
@@ -14,25 +15,28 @@ AUTHORS:
 
 #include "prob.h"
 
-
-/* http://www.linuxjournal.com/article.php?sid=6467 */
 double calc_prob(uint good, uint bad)
 {
     int n = good + bad;
-    double fw;
+    double fw, pw;
+
+    /* http://www.linuxjournal.com/article.php?sid=6467 */
+    
+    /* robs is Robinson's s parameter, the "strength of background info" */
+    /* robx is Robinson's x parameter, the assumed probability that
+     * a word we don't have enough info about will be spam */
+    /* n is the number of messages that contain the word w */
 
     if (n == 0)
 	fw = robx;
     else {
-	/* the original code had four divisions rather than one:
-	double pw = ((bad / msgs_bad) / (bad / msgs_bad + good / msgs_good));
+	/* The original version of this code has four divisions.
+	pw = ((bad / msgs_bad) / (bad / msgs_bad + good / msgs_good));
 	*/
-	double pw = bad*msgs_good / (bad*msgs_good + good*msgs_bad);
 
-	/* robs is Robinson's s parameter, the "strength of background info" */
-	/* robx is Robinson's x parameter, the assumed probability that
-	 * a word we don't have enough info about will be spam */
-	/* n is the amount of mails that contain the word w */
+	/* This modified version, with 1 division, is considerably% faster. */
+	pw = bad * msgs_good / (bad * msgs_good + good * msgs_bad);
+
 	fw = (robs * robx + n * pw) / (robs + n);
     }
 
