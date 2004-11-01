d73503fe12f14f3a441f7a2b9e6fafb8ee6dce21
m-a <m-a@046ba1d7-8659-4f4f-8137-08d775d845e8>
2004-11-01 00:50:37 +0000

    Source: Add copyright notice. User interface: allow user to pass new database and backup file name; if the latter is given, the database is upgraded "in place", by renaming it via a temporary file. Portability: use EXIT_FAILURE rather than a hardcoded 1. Bugfix: add room for the trailing NUL byte when generating the new_name. Bugfix: use dpgetflags() and check if the database is in Hash or B+ tree format, refuse work if B+ tree, to avoid corrupting the database. Bugfix: check every return value and print QDBM error message. Cleanup: remove "Done." message - UNIX programs don't print success.
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/trunk/bogofilter@4906 046ba1d7-8659-4f4f-8137-08d775d845e8

--- src/bogoQDBMupgrade.c
+++ src/bogoQDBMupgrade.c
@@ -1,47 +1,46 @@
 /*
-David Relson wrote:
+  bogoQDBMupgrade.c -- convert QDBM data base from Hash to B+Tree format
+  Copyright (C) 2004  Stefan Bellon
 
-> Use these commands:
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or (at
+  your option) any later version.
 
-> 1:  bogoutil -d /your/path/wordlist.db > wordlist.txt
-> 2:  mv /your/path/wordlist.db /your/path/wordlist.db.old
-> 3:  install new bogofilter
-> 4:  bogoutil -l /your/path/wordlist.db < wordlist.txt
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
 
-I hope I have simplified this a bit. I wrote the attached small
-converter which will convert any Depot database in a Villa database as
-long as the compare function of the Villa database is this cmpkey()
-function. In particular, Bogofilter's database can be converted to the
-new format even *after* you installed version 0.93.0.
-
-I hereby put it under the GNU GPL and you can include it together with
-some notes in the 0.93.0 release (after some testing ;-)
-
--- 
-Stefan Bellon
-
-Content-Type: text/plain; Name="qdbm_dp2vl.c"
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
 */
+
+/* additional error checks and cleanups by Matthias Andree */
+
 #include <stdio.h>
 #include <string.h>
+#include <errno.h>
+#include <unistd.h>
 
 #include <depot.h>
 #include <cabin.h>
 #include <villa.h>
 #include <stdlib.h>
 
-int cmpkey(const char *aptr, int asiz, const char *bptr, int bsiz)
+static int cmpkey(const char *aptr, int asiz, const char *bptr, int bsiz)
 {
     int aiter, biter;
-    
+
     for (aiter = 0, biter = 0; aiter < asiz && biter < bsiz; ++aiter, ++biter) {
-        if (aptr[aiter] != bptr[biter])
-            return (aptr[aiter] < bptr[biter]) ? -1 : 1;
+	if (aptr[aiter] != bptr[biter])
+	    return (aptr[aiter] < bptr[biter]) ? -1 : 1;
     }
-    
+
     if (aiter == asiz && biter == bsiz)
-        return 0;
-    
+	return 0;
+
     return (aiter == asiz) ? -1 : 1;
 }
 
@@ -50,66 +49,146 @@ int main(int argc, char *argv[])
     DEPOT *dho;
     VILLA *dhn;
     char *new_name;
+#define TACKON "-new"
+    const char *tackon = TACKON;
 
     int ret;
     int ksiz, dsiz;
     char *key, *data;
 
-    if (argc != 2) {
-        fprintf(stderr, "Usage:  qdbm_dp2vl database\n");
-        exit(1);
+    if (argc < 2 || argc > 4) {
+	fprintf(stderr,
+		"Usage: %s <database> [<new database>]\n"
+		"   or: %s <database> <tempfile> <backup>\n"
+		"  <new database> defaults to <database>" TACKON "\n"
+		"  If <backup> is given, <database> is backed up to <backup> and\n"
+		"  upgraded in place, via <tempfile>.\n",
+		argv[0], argv[0]);
+	exit(EXIT_FAILURE);
     }
-    
+
     dho = dpopen(argv[1], DP_OREADER, 0);
     if (!dho) {
-        fprintf(stderr, "Couldn't open database '%s'.\n", argv[1]);
-        exit(1);
+	fprintf(stderr, "Couldn't open database '%s': %s\n", argv[1],
+		dperrmsg(dpecode));
+	exit(EXIT_FAILURE);
     }
-    
-    new_name = malloc(strlen(argv[1])+4);
+
+    if (dpgetflags(dho) & 1) {
+	fprintf(stderr, "Database '%s' is already in B+ tree format.\n", argv[1]);
+	dpclose(dho);
+	exit(EXIT_FAILURE);
+    }
+
+    if (argc >= 3) {
+	new_name = strdup(argv[2]);
+    } else {
+	new_name = malloc(strlen(argv[1]) + strlen(tackon) + 1);
+	if (new_name) {
+	    strcpy(new_name, argv[1]);
+	    strcat(new_name, tackon);
+	}
+    }
+
     if (!new_name) {
-        fprintf(stderr, "Couldn't allocate memory.\n");
-        dpclose(dho);
-        exit(1);
+	fprintf(stderr, "Couldn't allocate memory.\n");
+	dpclose(dho);
+	exit(EXIT_FAILURE);
     }
-    
-    new_name = strcpy(new_name, argv[1]);
-    new_name = strcat(new_name, "-new");
+
+    if (strcmp(new_name, argv[1]) == 0) {
+	fprintf(stderr, "database and %s must be different files!\n",
+		argc >= 4 ? "tempfile" : "new database");
+	dpclose(dho);
+	free(new_name);
+	exit(EXIT_FAILURE);
+    }
+
+    remove(new_name); /* start with a fresh data base */
 
     dhn = vlopen(new_name, VL_OWRITER | VL_OCREAT, cmpkey);
     if (!dhn) {
-        fprintf(stderr, "Couldn't create database '%s'.\n", new_name);
-        free(new_name);
-        dpclose(dho);
-        exit(1);
+	fprintf(stderr, "Couldn't create database '%s': %s\n", new_name,
+		dperrmsg(dpecode));
+	dpclose(dho);
+	remove(new_name);
+	free(new_name);
+	exit(EXIT_FAILURE);
     }
-    
+
     ret = dpiterinit(dho);
     if (ret) {
-        while ((key = dpiternext(dho, &ksiz))) {
-            data = dpget(dho, key, ksiz, 0, -1, &dsiz);
-            if (data) {
-                ret = vlput(dhn, key, ksiz, data, dsiz, VL_DOVER);
-                if (!ret) {
-                    fprintf(stderr, "Error writing key '%.*s', value '%.*s'\n",
-                            ksiz, key, dsiz, data);
-                }
-                free(data);
-            }
-            free(key);
-        }
+	while ((key = dpiternext(dho, &ksiz))) {
+	    data = dpget(dho, key, ksiz, 0, -1, &dsiz);
+	    if (data) {
+		ret = vlput(dhn, key, ksiz, data, dsiz, VL_DOVER);
+		if (!ret) {
+		    int i;
+		    fprintf(stderr, "Error writing key '%.*s', value '0x",
+			    ksiz, key);
+		    for (i = 0 ; i < dsiz ; i++)
+			fprintf(stderr, "%02x", data[i]);
+		    fprintf(stderr, "': %s\n", dperrmsg(dpecode));
+		    free(data);
+		    free(key);
+		    goto barf;
+		}
+		free(data);
+	    } else {
+		fprintf(stderr, "Error reading value for key '%.*s': %s\n",
+			ksiz, key, dperrmsg(dpecode));
+		free(key);
+		goto barf;
+	    }
+	    free(key);
+	}
     } else {
-        fprintf(stderr, "Error creating database iterator.\n");
-        free(new_name);
-        dpclose(dho);
-        vlclose(dhn);
-        exit(1);
+	fprintf(stderr, "Error creating database iterator: %s\n",
+		dperrmsg(dpecode));
+	goto barf;
     }
-    
-    dpclose(dho);
-    vlclose(dhn);
-    
-    fprintf(stderr, "Done.\n");
 
-    return 0;
+    if (dpfatalerror(dho)) {
+	fprintf(stderr, "Error: database has fatal error state after reading.\n"
+		"Last error was: %s", dperrmsg(dpecode));
+	goto barf;
+    }
+
+    if (!dpclose(dho)) {
+	fprintf(stderr, "Error closing input database: %s\n", dperrmsg(dpecode));
+	goto barf;
+    }
+    if (!vlclose(dhn)) {
+	fprintf(stderr, "Error closing output database: %s\n", dperrmsg(dpecode));
+	goto barf;
+    }
+
+    if (argc >= 4) {
+	/* rename output to input, taking care the file is always
+	 * present */
+	remove(argv[3]); /* ignore errors */
+	if (link(argv[1], argv[3])) {
+	    fprintf(stderr, "Error: cannot link %s to %s: %s\n",
+		    argv[1], argv[3], strerror(errno));
+	    free(new_name);
+	    exit(EXIT_FAILURE);
+	}
+
+	if (rename(new_name, argv[1])) {
+	    fprintf(stderr, "Error: cannot rename %s to %s: %s\n",
+		    new_name, argv[1], strerror(errno));
+	    free(new_name);
+	    exit(EXIT_FAILURE);
+	}
+    }
+
+    exit(EXIT_SUCCESS);
+
+    /* clean up in case of error */
+barf:
+    vlclose(dhn);
+    remove(new_name);
+    free(new_name);
+    dpclose(dho);
+    exit(EXIT_FAILURE);
 }
