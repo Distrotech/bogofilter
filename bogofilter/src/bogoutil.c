099a70e17bd99bd73925751f47b8fc149e63e951
relson <relson@046ba1d7-8659-4f4f-8137-08d775d845e8>
2004-11-11 01:44:33 +0000

    Add support for db_lk_max_locks=N and db_lk_max_objects=N to bogofilter, bogofilter.cf, and bogoutil
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/trunk@4967 046ba1d7-8659-4f4f-8137-08d775d845e8

--- bogofilter/src/bogoutil.c
+++ bogofilter/src/bogoutil.c
@@ -26,7 +26,9 @@ AUTHORS:
 #include "bogohist.h"
 #include "bogohome.h"
 #include "buff.h"
+#include "configfile.h"
 #include "datastore.h"
+#include "datastore_db.h"
 #include "error.h"
 #include "maint.h"
 #include "msgcounts.h"
@@ -44,6 +46,10 @@ static int token_count = 0;
 
 bool  maintain = false;
 bool  onlyprint = false;
+
+/* Function Prototypes */
+
+static int process_arg(int option, char *name, const char *arg);
 
 /* Function Definitions */
 
@@ -481,6 +487,53 @@ static cmd_t flag = M_NONE;
 
 #define	OPTIONS	":a:c:d:Df:F:hH:I:k:l:m:np:r:R:s:u:vVw:x:X:y:"
 
+struct option long_options[] = {
+#ifdef	HAVE_DECL_DB_CREATE
+    { "db_lk_max_locks",		R, 0, O_DB_MAX_LOCKS },
+    { "db_lk_max_objects",		R, 0, O_DB_MAX_OBJECTS },
+#ifdef	FUTURE_DB_OPTIONS
+    { "db_log_autoremove",		R, 0, O_DB_LOG_AUTOREMOVE },
+    { "db_txn_durable",			R, 0, O_DB_TXN_DURABLE },
+#endif
+#endif
+    { "debug-flags",			R, 0, 'x' },
+    { "debug-to-stdout",		N, 0, 'D' },
+    { "verbosity",			N, 0, 'v' },
+    /* The following options are present to allow config files
+    ** to be read without complaints (and mostly ignored)
+    */
+    { "config-file",			N, 0, O_IGNORE },
+    { "no-config-file",			N, 0, O_IGNORE },
+    { "no-header-tags",			N, 0, O_IGNORE },
+    { "input-file",			N, 0, O_IGNORE },
+    { "output-file",			N, 0, O_IGNORE },
+    { "query",				N, 0, O_IGNORE },
+    { "block_on_subnets",		R, 0, O_IGNORE },
+    { "bogofilter_dir",			R, 0, O_IGNORE },
+    { "charset_default",		R, 0, O_IGNORE },
+    { "ham_cutoff",			R, 0, O_IGNORE },
+    { "header_format",			R, 0, O_IGNORE },
+    { "log_header_format",		R, 0, O_IGNORE },
+    { "log_update_format",		R, 0, O_IGNORE },
+    { "min_dev",			R, 0, O_IGNORE },
+    { "replace_nonascii_characters",	R, 0, O_IGNORE },
+    { "robs",				R, 0, O_IGNORE },
+    { "robx",				R, 0, O_IGNORE },
+    { "spam_cutoff",			R, 0, O_IGNORE },
+    { "spam_header_name",		R, 0, O_IGNORE },
+    { "spam_subject_tag",		R, 0, O_IGNORE },
+    { "spamicity_formats",		R, 0, O_IGNORE },
+    { "spamicity_tags",			R, 0, O_IGNORE },
+    { "stats_in_header",		R, 0, O_IGNORE },
+    { "terse",				R, 0, O_IGNORE },
+    { "terse_format",			R, 0, O_IGNORE },
+    { "thresh_update",			R, 0, O_IGNORE },
+    { "timestamp",			R, 0, O_IGNORE },
+    { "unsure_subject_tag",		R, 0, O_IGNORE },
+    { "user_config_file",		R, 0, O_IGNORE },
+    { NULL,				0, 0, 0 }
+};
+
 static int process_arglist(int argc, char **argv)
 {
     int option;
@@ -494,171 +547,202 @@ static int process_arglist(int argc, char **argv)
     _wildcard (&argc, &argv);	/* expand wildcards (*.*) */
 #endif
 
-    while ((option = getopt(argc, argv, OPTIONS)) != -1)
-	switch (option) {
-	case '?':
-	    fprintf(stderr, "Unknown option -%c.\n", optopt);
-	    break;
+    while (1)
+    {
+	int option_index = 0;
+	int this_option_optind = optind ? optind : 1;
+	char *name;
 
-	case 'f':
-	    flag = M_RECOVER;
-	    count += 1;
-	    ds_file = (char *) optarg;
-	    break;
+	option = getopt_long(argc, argv, OPTIONS,
+			     long_options, &option_index);
 
-	case 'F':
-	    flag = M_CRECOVER;
-	    count += 1;
-	    ds_file = (char *) optarg;
-	    break;
+	if (option == -1)
+ 	    break;
 
-	case 'd':
-	    flag = M_DUMP;
-	    count += 1;
-	    ds_file = (char *) optarg;
-	    break;
-
-	case 'k':
-	    db_cachesize=(uint) atoi(optarg);
-	    break;
-
-	case 'l':
-	    flag = M_LOAD;
-	    count += 1;
-	    ds_file = (char *) optarg;
-	    break;
-
-	case 'm':
-	    flag = M_MAINTAIN;
-	    count += 1;
-	    ds_file = (char *) optarg;
-	    break;
-
-	case 'p':
-	    prob = true;
-	    /*@fallthrough@*/
-
-	case 'w':
-	    flag = M_WORD;
-	    count += 1;
-	    ds_file = (char *) optarg;
-	    break;
-
-	case 'r':
-	    onlyprint = true;
-	case 'R':
-	    flag = M_ROBX;
-	    count += 1;
-	    ds_file = (char *) optarg;
-	    break;
-
-	case 'u':
-	    upgrade_wordlist_version = true;
-	    flag = M_MAINTAIN;
-	    count += 1;
-	    ds_file = (char *) optarg;
-	    break;
-
-	case 'v':
-	    verbose++;
-	    break;
-
-	case ':':
-	    fprintf(stderr, "Option -%c requires an argument.\n", optopt);
-	    exit(EX_ERROR);
-
-	case 'h':
-	    help();
-	    exit(EX_OK);
-
-	case 'H':
-	    flag = M_HIST;
-	    count += 1;
-	    ds_file = (char *) optarg;
-	    break;
-
-	case 'V':
-	    print_version();
-	    exit(EX_OK);
-
-	case 'x':
-	    set_debug_mask( (char *) optarg );
-	    break;
-
-	case 'X':
-	    set_bogotest( optarg );
-	    break;
-
-	case 'a':
-	    maintain = true;
-	    thresh_date = string_to_date(optarg);
-	    break;
-
-	case 'c':
-	    maintain = true;
-	    thresh_count = (uint) atoi((char *)optarg);
-	    break;
-
-	case 's':
-	{
-	    unsigned long mi, ma;
-
-	    maintain = true;
-	    
-	    if (2 == sscanf((const char *)optarg, "%lu,%lu", &mi, &ma)) {
-		size_min = mi;
-		size_max = ma;
-	    } else {
-		fprintf(stderr, "syntax error in argument \"%s\" of -s\n.",
-			optarg);
-		exit(EX_ERROR);
-	    }
-	}
-	break;
-
-	case 'n':
-	    maintain = true;
-	    replace_nonascii_characters ^= true;
-	    break;
-
-	case 'y':		/* date as YYYYMMDD */
-	{
-	    YYYYMMDD date = string_to_date(optarg);
-	    maintain = true;
-	    if (date != 0 && date < 19990000) {
-		fprintf(stderr, "Date format for '-y' option is YYYYMMDD\n");
-		exit(EX_ERROR);
-	    }
-	    set_date( date );
-	    break;
-	}
-
-	case 'I':
-	    fpin = fopen( optarg, "r" );
-	    if (fpin == NULL) {
-		fprintf(stderr, "Can't read file '%s'\n", optarg);
-		exit(EX_ERROR);
-	    }
-	    break;
-
-	case 'D':
-	    dbgout = stdout;
-	    break;
-
-	default:
-	    abort();
-	}
+	name = (option_index == 0) ? argv[this_option_optind] : long_options[option_index].name;
+	count += process_arg(option, name, optarg);
+    }
 
     if (count != 1)
     {
-      fprintf(stderr, "%s: Exactly one of the -d, -f, -F, -l, -R or -w flags "
-	      "must be present.\n", progname);
-      exit(EX_ERROR);
+	fprintf(stderr, "%s: Exactly one of the -d, -f, -F, -l, -R or -w flags "
+		"must be present.\n", progname);
+	exit(EX_ERROR);
     }
-
+ 
     if (optind < argc && flag != M_WORD) {
 	fprintf(stderr, "Extra arguments given, first: %s. Aborting.\n", argv[optind]);
 	exit(EX_ERROR);
+    }
+
+    return count;
+}
+
+static int process_arg(int option, char *name, const char *val)
+{
+    int count = 0;
+
+    switch (option) {
+    case '?':
+	fprintf(stderr, "Unknown option '%s'.\n", name);
+	break;
+
+    case 'f':
+	flag = M_RECOVER;
+	count += 1;
+	ds_file = (char *) optarg;
+	break;
+
+    case 'F':
+	flag = M_CRECOVER;
+	count += 1;
+	ds_file = (char *) optarg;
+	break;
+
+    case 'd':
+	flag = M_DUMP;
+	count += 1;
+	ds_file = (char *) optarg;
+	break;
+
+    case 'k':
+	db_cachesize=(uint) atoi(optarg);
+	break;
+
+    case 'l':
+	flag = M_LOAD;
+	count += 1;
+	ds_file = (char *) optarg;
+	break;
+
+    case 'm':
+	flag = M_MAINTAIN;
+	count += 1;
+	ds_file = (char *) optarg;
+	break;
+
+    case 'p':
+	prob = true;
+	/*@fallthrough@*/
+
+    case 'w':
+	flag = M_WORD;
+	count += 1;
+	ds_file = (char *) optarg;
+	break;
+
+    case 'r':
+	onlyprint = true;
+    case 'R':
+	flag = M_ROBX;
+	count += 1;
+	ds_file = (char *) optarg;
+	break;
+
+    case 'u':
+	upgrade_wordlist_version = true;
+	flag = M_MAINTAIN;
+	count += 1;
+	ds_file = (char *) optarg;
+	break;
+
+    case 'v':
+	verbose++;
+	break;
+
+    case ':':
+	fprintf(stderr, "Option -%c requires an argument.\n", optopt);
+	exit(EX_ERROR);
+
+    case 'h':
+	help();
+	exit(EX_OK);
+
+    case 'H':
+	flag = M_HIST;
+	count += 1;
+	ds_file = (char *) optarg;
+	break;
+
+    case 'V':
+	print_version();
+	exit(EX_OK);
+
+    case 'x':
+	set_debug_mask( (char *) optarg );
+	break;
+
+    case 'X':
+	set_bogotest( optarg );
+	break;
+
+    case 'a':
+	maintain = true;
+	thresh_date = string_to_date(optarg);
+	break;
+
+    case 'c':
+	maintain = true;
+	thresh_count = (uint) atoi((char *)optarg);
+	break;
+
+    case 's':
+    {
+	unsigned long mi, ma;
+
+	maintain = true;
+	    
+	if (2 == sscanf((const char *)optarg, "%lu,%lu", &mi, &ma)) {
+	    size_min = mi;
+	    size_max = ma;
+	} else {
+	    fprintf(stderr, "syntax error in argument \"%s\" of -s\n.",
+		    optarg);
+	    exit(EX_ERROR);
+	}
+    }
+    break;
+
+    case 'n':
+	maintain = true;
+	replace_nonascii_characters ^= true;
+	break;
+
+    case 'y':		/* date as YYYYMMDD */
+    {
+	YYYYMMDD date = string_to_date(optarg);
+	maintain = true;
+	if (date != 0 && date < 19990000) {
+	    fprintf(stderr, "Date format for '-y' option is YYYYMMDD\n");
+	    exit(EX_ERROR);
+	}
+	set_date( date );
+	break;
+    }
+
+    case 'I':
+	fpin = fopen( optarg, "r" );
+	if (fpin == NULL) {
+	    fprintf(stderr, "Can't read file '%s'\n", optarg);
+	    exit(EX_ERROR);
+	}
+	break;
+
+    case 'D':
+	dbgout = stdout;
+	break;
+
+#ifdef	HAVE_DECL_DB_CREATE
+    case O_DB_MAX_OBJECTS:	db_max_objects = atoi(val);		  break;
+    case O_DB_MAX_LOCKS:	db_max_locks   = atoi(val);		  break;
+#ifdef	FUTURE_DB_OPTIONS
+    case O_DB_LOG_AUTOREMOVE:	db_log_autoremove  = get_bool(name, val); break;
+    case O_DB_TXN_DURABLE:	db_txn_durable = get_bool(name, val);	  break;
+#endif
+#endif
+
+    default:
+	abort();
     }
 
     return count;
