7aeaeed37c74f404575d022cd6cb387ebea7668d
m-a <m-a@046ba1d7-8659-4f4f-8137-08d775d845e8>
2004-11-22 21:12:25 +0000

    Don't run ds_init twice. Breaks locking.
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/trunk@5042 046ba1d7-8659-4f4f-8137-08d775d845e8

--- bogofilter/src/wordlists.c
+++ bogofilter/src/wordlists.c
@@ -154,8 +154,6 @@ void open_wordlists(wordlist_t *list, dbmode_t mode)
     LIST_INIT(&envs);
 
     while (retry) {
-	ds_init();
-
 	if (run_type & (REG_SPAM | REG_GOOD | UNREG_SPAM | UNREG_GOOD))
 	    retry = open_wordlist(get_default_wordlist(list), mode);
 	else {
