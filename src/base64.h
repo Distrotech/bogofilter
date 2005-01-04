91a97ab4de06f530e8f1decde767f802bb841a2d
m-a <m-a@046ba1d7-8659-4f4f-8137-08d775d845e8>
2005-01-04 15:46:33 +0000

    Document base64 decoder.
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/trunk/bogofilter@5267 046ba1d7-8659-4f4f-8137-08d775d845e8

--- src/base64.h
+++ src/base64.h
@@ -1,18 +1,18 @@
 /* $Id$ */
 
-/*****************************************************************************
-
-NAME:
-   base64.h -- prototypes and definitions for base64.c
-
-******************************************************************************/
+/** \file base64.h
+ * prototypes and definitions for base64.c
+ */
 
 #ifndef	HAVE_BASE64_H
 #define	HAVE_BASE64_H
 
 #include "word.h"
 
-uint	base64_decode(word_t *word);
+/** decode base64 in-place, \return the count of decoded bytes in the \a
+ * word */
+uint	base64_decode(word_t *word /** string to decode in-place, will be modified */);
+/** check if \a word contains conformant base64-encoded data */
 bool	base64_validate(const word_t *word);
 
 #endif	/* HAVE_BASE64_H */
