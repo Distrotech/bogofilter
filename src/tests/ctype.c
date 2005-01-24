ec60ba917c06c80b9888cf98a6a2e519bb7b2670
relson <relson@046ba1d7-8659-4f4f-8137-08d775d845e8>
2005-01-24 04:37:42 +0000

    Check overall correctness of isspace().
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/trunk/bogofilter@5410 046ba1d7-8659-4f4f-8137-08d775d845e8

--- src/tests/ctype.c
+++ src/tests/ctype.c
@@ -1,8 +1,8 @@
 /** \file ctype.c
- * This file tests if isspace(0xa0) is conformant and returns false, in
- * which case we exit with EXIT_SUCCESS (0);
- * on broken systems such as OpenBSD 2.9 to 3.6 that return true for
- * isspace(0xa0), exit with EXIT_FAILURE.
+ * This file tests whether isspace() agrees with the POSIX definition of whitespace.
+ * If so, it exits with EXIT_SUCCESS (0).
+ * On broken systems such as OpenBSD 2.9 to 3.6 that return true for
+ * isspace(0xa0), it will exit with EXIT_FAILURE.
  * \author Matthias Andree
  * \date 2005
  *
@@ -10,10 +10,24 @@
  */
 
 #include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+const char POSIX_space[] = " \f\n\r\t\v";
 
 int main(void) {
-    if (isspace(0xa0))
-	exit(EXIT_FAILURE);
+    int i;
+
+    for (i=1; i < 256; i += 1) {
+	int posix = strchr(POSIX_space, i) != NULL;
+	int space = isspace(i) != 0;
+	if (posix != space) {
+	    fprintf( stderr, "Error at %d.  posix: %d, space: %d\n", i, posix, space );
+	    fflush(stderr);
+	    exit(EXIT_FAILURE);
+	}
+    }
+
     exit(EXIT_SUCCESS);
 }
