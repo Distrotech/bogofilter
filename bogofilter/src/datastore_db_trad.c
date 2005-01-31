c7304f28ad0c38a3ce24140edd18f5f4a462071e
relson <relson@046ba1d7-8659-4f4f-8137-08d775d845e8>
2005-01-31 12:50:44 +0000

    Cleanup header comments.
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/branches/RTS@5473 046ba1d7-8659-4f4f-8137-08d775d845e8

--- bogofilter/src/datastore_db_trad.c
+++ bogofilter/src/datastore_db_trad.c
@@ -3,37 +3,16 @@
 /*****************************************************************************
 
 NAME:
-datastore_db.c -- implements the datastore, using Berkeley DB
+datastore_db_trad.c -- implements bogofilter's traditional
+		       (non-transactional) datastore, 
+		       using Berkeley DB
 
 AUTHORS:
 Gyepi Sam <gyepi@praxis-sw.com>   2002 - 2003
 Matthias Andree <matthias.andree@gmx.de> 2003 - 2004
+David Relson	<relson@osagesoftware.com> 2003 - 2005
 
 ******************************************************************************/
-
-/* To avoid header file conflicts the order is:
-**	1. System header files
-**	2. Header files for external packages
-**	3. Bogofilter's header files
-*/
-
-/* This code has been tested with BerkeleyDB 3.1 3.2, 3.3, 4.0,
- * 4.1 and 4.2.  -- Matthias Andree, 2004-11-29 */
-
-/* TODO:
- * - implement proper retry when our transaction is aborted after a
- *   deadlock
- * - document code changes
- * - conduct massive tests
- * - check if we really need the log files for "catastrophic recovery"
- *   or if we can remove them (see db_archive documentation)
- *   as the log files are *HUGE* even compared with the data base
- */
-
-/*
- * NOTE: this code is an "#if" nightmare due to the many different APIs
- * in the many different BerkeleyDB versions.
- */
 
 #define DONT_TYPEDEF_SSIZE_T 1
 #include "common.h"
