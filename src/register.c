23f6663881bc152f2d6fdb6ee97f89e5de2cb4f8
m-a <m-a@046ba1d7-8659-4f4f-8137-08d775d845e8>
2005-06-06 02:14:07 +0000

    Bump retry count massively to get t.lock3 working.
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/trunk/bogofilter@6028 046ba1d7-8659-4f4f-8137-08d775d845e8

--- src/register.c
+++ src/register.c
@@ -29,8 +29,8 @@ void register_words(run_t _run_type, wordhash_t *h, u_int32_t msgcount)
     hashnode_t *node;
     wordprop_t *wordprop;
     run_t save_run_type = run_type;
-    int retrycount = 12;		/* we'll retry an aborted
-					   registration a dozen times
+    int retrycount = 60;		/* we'll retry an aborted
+					   registration five dozen times
 					   before giving up. */
     bool first;
 
