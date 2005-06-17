2258f2f239c5fc0af8053bacdc923025772e1022
relson <relson@046ba1d7-8659-4f4f-8137-08d775d845e8>
2005-06-17 03:39:08 +0000

    Add debug code.
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/trunk/bogofilter@6061 046ba1d7-8659-4f4f-8137-08d775d845e8

--- src/convert_unicode.c
+++ src/convert_unicode.c
@@ -122,6 +122,8 @@ void init_charset_table_iconv(const char *charset_name)
 	iconv_close(cd);
 
 /*  iconv_t iconv_open(const char *tocode, const char *fromcode); */
+    if (DEBUG_ICONV(1))
+	fprintf(dbgout, "converting %s to %s\n", charset_default, charset_name);
     cd = iconv_open( charset_default, charset_name );
     if (cd == (iconv_t)(-1)) {
 	int err = errno;
