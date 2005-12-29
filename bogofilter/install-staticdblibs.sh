2aa00222946351fbc1e054d5f808cd78296c9b0f
relson <relson@046ba1d7-8659-4f4f-8137-08d775d845e8>
2005-12-29 16:12:15 +0000

    Indent conditionals.
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/trunk@6384 046ba1d7-8659-4f4f-8137-08d775d845e8

--- bogofilter/install-staticdblibs.sh
+++ bogofilter/install-staticdblibs.sh
@@ -47,14 +47,16 @@ fetch() {
 # _md5_file FILE
 # check MD5 sum of a single file
 _md5_file() {
-    if command -v >/dev/null 2>&1 md5sum ; then
+    if command -v >/dev/null 2>&1 md5sum
+    then
 	md5sum "$1" | cut -f1 -d" "
-	else if command -v >/dev/null 2>&1 md5 ; then
-	    md5 -q "$1"
-	else
-	    echo >&2 "Cannot calculate md5 hash, no program found."
-	    exit 1
-	fi
+    else if command -v >/dev/null 2>&1 md5
+    then
+	md5 -q "$1"
+    else
+	echo >&2 "Cannot calculate md5 hash, no program found."
+	exit 1
+    fi
     fi
 }
 
@@ -81,12 +83,12 @@ sqpfx=/opt/sqlite-3-lean
 ### download SleepyCat DB 4.2.52 and patches
 build_db=0
 if test ! -f $dbpfx/lib/libdb.a ; then
-want http://downloads.sleepycat.com/db-4.2.52.tar.gz       cbc77517c9278cdb47613ce8cb55779f
-want http://www.sleepycat.com/update/4.2.52/patch.4.2.52.1 1227f5f9ff43d48b5b1759e113a1c2d7
-want http://www.sleepycat.com/update/4.2.52/patch.4.2.52.2 3da7efd8d29919a9113e2f6f5166f5b7
-want http://www.sleepycat.com/update/4.2.52/patch.4.2.52.3 0bf9ebbe852652bed433e522928d40ec
-want http://www.sleepycat.com/update/4.2.52/patch.4.2.52.4 9cfeff4dce0c11372c0b04b134f8faef
-build_db=1
+    want http://downloads.sleepycat.com/db-4.2.52.tar.gz       cbc77517c9278cdb47613ce8cb55779f
+    want http://www.sleepycat.com/update/4.2.52/patch.4.2.52.1 1227f5f9ff43d48b5b1759e113a1c2d7
+    want http://www.sleepycat.com/update/4.2.52/patch.4.2.52.2 3da7efd8d29919a9113e2f6f5166f5b7
+    want http://www.sleepycat.com/update/4.2.52/patch.4.2.52.3 0bf9ebbe852652bed433e522928d40ec
+    want http://www.sleepycat.com/update/4.2.52/patch.4.2.52.4 9cfeff4dce0c11372c0b04b134f8faef
+    build_db=1
 fi
 
 ### download SQLite 3.2.8
@@ -98,58 +100,58 @@ build_sqlite=0
 if test ! -f $sqpfx/lib/libsqlite3.a || \
     objdump -t /opt/sqlite-3-lean/lib/libsqlite3.a \
 	| grep -q __ctype_b_loc ; then
-want http://www.sqlite.org/sqlite-3.2.8.tar.gz             9f2c014aaa46565b8163e047166a5686
-build_sqlite=1
+    want http://www.sqlite.org/sqlite-3.2.8.tar.gz             9f2c014aaa46565b8163e047166a5686
+    build_sqlite=1
 fi
 
 # build DB 4.2
 if test $build_db = 1 ; then
-rm -rf $dbdir
-gunzip -c -d $dbdir.tar.gz | tar xf -
-patch -s -d $dbdir -p0 <patch.4.2.52.1
-patch -s -d $dbdir -p0 <patch.4.2.52.2
-patch -s -d $dbdir -p0 <patch.4.2.52.3
-patch -s -d $dbdir -p0 <patch.4.2.52.4
-echo "installing $dbdir into $dbpfx"
-cd $dbdir/build_unix
-env CPPFLAGS=-D__NO_CTYPE ../dist/configure \
-  --prefix=$dbpfx --silent \
-  --with-mutex=x86/gcc-assembly \
-  --disable-cxx --disable-shared \
-  --disable-queue --disable-replication \
-  --disable-hash  --disable-cryptography
-make -s
-sudo make -s install_setup install_include install_lib
-# fix permissions:
-sudo chown -R 0:0 $dbpfx
-sudo chmod -R a-w $dbpfx
-# save some kBytes:
-sudo ln -f $dbpfx/lib/libdb-4.2.a $dbpfx/lib/libdb.a
-echo "$dbdir was installed into $dbpfx"
-cd - >/dev/null
-rm -rf $dbdir
+    rm -rf $dbdir
+    gunzip -c -d $dbdir.tar.gz | tar xf -
+    patch -s -d $dbdir -p0 <patch.4.2.52.1
+    patch -s -d $dbdir -p0 <patch.4.2.52.2
+    patch -s -d $dbdir -p0 <patch.4.2.52.3
+    patch -s -d $dbdir -p0 <patch.4.2.52.4
+    echo "installing $dbdir into $dbpfx"
+    cd $dbdir/build_unix
+    env CPPFLAGS=-D__NO_CTYPE ../dist/configure \
+	--prefix=$dbpfx --silent \
+	--with-mutex=x86/gcc-assembly \
+	--disable-cxx --disable-shared \
+	--disable-queue --disable-replication \
+	--disable-hash  --disable-cryptography
+    make -s
+    sudo make -s install_setup install_include install_lib
+    # fix permissions:
+    sudo chown -R 0:0 $dbpfx
+    sudo chmod -R a-w $dbpfx
+    # save some kBytes:
+    sudo ln -f $dbpfx/lib/libdb-4.2.a $dbpfx/lib/libdb.a
+    echo "$dbdir was installed into $dbpfx"
+    cd - >/dev/null
+    rm -rf $dbdir
 fi
 
 # build SQLite 3
 if test $build_sqlite = 1 ; then
-rm -rf build-$sqdir $sqdir
-gunzip -cd $sqdir.tar.gz | tar xf -
-set -e
-echo "installing $sqdir"
-mkdir -p build-$sqdir
-cd build-$sqdir
-env CFLAGS="-O2 -D__NO_CTYPE" ../$sqdir/configure \
-  --prefix=$sqpfx --silent \
-  --disable-shared --disable-tcl \
-  --disable-threadsafe
-make -s
-sudo make -s install
-# fix permissions:
-sudo chown -R 0:0 $sqpfx
-sudo chmod -R a-w $sqpfx
-echo "$sqdir was installed into $sqpfx"
-cd - >/dev/null
-rm -rf build-$sqdir $sqdir
+    rm -rf build-$sqdir $sqdir
+    gunzip -cd $sqdir.tar.gz | tar xf -
+    set -e
+    echo "installing $sqdir"
+    mkdir -p build-$sqdir
+    cd build-$sqdir
+    env CFLAGS="-O2 -D__NO_CTYPE" ../$sqdir/configure \
+	--prefix=$sqpfx --silent \
+	--disable-shared --disable-tcl \
+	--disable-threadsafe
+    make -s
+    sudo make -s install
+    # fix permissions:
+    sudo chown -R 0:0 $sqpfx
+    sudo chmod -R a-w $sqpfx
+    echo "$sqdir was installed into $sqpfx"
+    cd - >/dev/null
+    rm -rf build-$sqdir $sqdir
 fi
 
 rm -f $dbdir.tar.gz $sqdir.tar.gz patch.4.2.52.?
