e69e63e3f5fd14ae91cfb438f9a683acde7bf42f
relson <relson@046ba1d7-8659-4f4f-8137-08d775d845e8>
2006-11-23 15:44:17 +0000

    Revise copyright messages.
    git-svn-id: svn://svn.code.sf.net/p/bogofilter/code/trunk@6584 046ba1d7-8659-4f4f-8137-08d775d845e8

--- bogofilter/src/bogoconfig.c
+++ bogofilter/src/bogoconfig.c
@@ -389,8 +389,8 @@ static void print_version(void)
     (void)fprintf(stdout,
 		  "%s version %s\n"
 		  "    Database: %s\n"
-		  "Copyright (C) 2002-2003 Eric S. Raymond, Adrian Otto, Gyepi Sam.\n"
-		  "Copyright (C) 2002-2006 David Relson, Matthias Andree, Greg Louis\n\n"
+		  "Copyright (C) 2002-2006 David Relson, Matthias Andree, Greg Louis\n"
+		  "Copyright (C) 2002-2003 Eric S. Raymond, Adrian Otto, Gyepi Sam.\n\n"
 		  "%s comes with ABSOLUTELY NO WARRANTY.  "
 		  "This is free software, and\nyou are welcome to "
 		  "redistribute it under the General Public License.  "
